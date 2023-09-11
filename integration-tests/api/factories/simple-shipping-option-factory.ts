adbaf3d255295888e990987655b6784f8a43ef00
Sammy filly <136061549+sammyfilly@users.noreply.github.com>
2023-09-11 21:36:09 +0100

    Initialize docs/links-bug

---
+++ integration-tests/api/factories/simple-shipping-option-factory.ts
@@ -0,0 +1,51 @@
+import {
+  ShippingOption,
+  ShippingOptionPriceType,
+  ShippingProfile,
+  ShippingProfileType,
+} from "@medusajs/medusa"
+import faker from "faker"
+import { Connection } from "typeorm"
+
+export type ShippingOptionFactoryData = {
+  name?: string
+  region_id: string
+  is_return?: boolean
+  is_giftcard?: boolean
+  price?: number
+  price_type?: ShippingOptionPriceType
+  data?: object
+}
+
+export const simpleShippingOptionFactory = async (
+  connection: Connection,
+  data: ShippingOptionFactoryData,
+  seed?: number
+): Promise<ShippingOption> => {
+  if (typeof seed !== "undefined") {
+    faker.seed(seed)
+  }
+
+  const manager = connection.manager
+  const defaultProfile = await manager.findOne(ShippingProfile, {
+    type: ShippingProfileType.DEFAULT,
+  })
+
+  const gcProfile = await manager.findOne(ShippingProfile, {
+    type: ShippingProfileType.GIFT_CARD,
+  })
+
+  const created = manager.create(ShippingOption, {
+    id: `simple-so-${Math.random() * 1000}`,
+    name: data.name || "Test Method",
+    is_return: data.is_return ?? false,
+    region_id: data.region_id,
+    provider_id: "test-ful",
+    profile_id: data.is_giftcard ? gcProfile.id : defaultProfile.id,
+    price_type: data.price_type ?? ShippingOptionPriceType.FLAT_RATE,
+    data: data.data ?? {},
+    amount: typeof data.price !== "undefined" ? data.price : 500,
+  })
+  const option = await manager.save(created)
+  return option
+}
