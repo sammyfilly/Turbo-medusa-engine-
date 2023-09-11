581923ce711e17de562ebcd297734ff3bd033763
Sammy filly <136061549+sammyfilly@users.noreply.github.com>
2023-09-11 21:29:52 +0100

    Initialize refactor-paymentPlugins

---
+++ packages/medusa-payment-adyen/src/services/mobilepay-adyen.js
@@ -0,0 +1,60 @@
+import { AbstractPaymentService } from "@medusajs/medusa"
+
+class MobilePayAdyenService extends AbstractPaymentService {
+  static identifier = "mobilepay-adyen"
+
+  constructor({ adyenService }) {
+    super({ adyenService })
+
+    this.adyenService_ = adyenService
+  }
+
+  async getStatus(paymentSessionData) {
+    return await this.adyenService_.getStatus(paymentSessionData)
+  }
+
+  async createPayment(cart) {
+    const raw = await this.adyenService_.createPayment(cart)
+    raw.type = "mobilepay"
+    return raw
+  }
+
+  async authorizePayment(paymentSessionData, context) {
+    return await this.adyenService_.authorizePayment(paymentSessionData, context)
+  }
+
+  async retrievePayment(paymentData) {
+    // TODO: This method does not exists on adyenService
+    return this.adyenService_.retrievePayment(paymentData)
+  }
+
+  async updatePayment(paymentSessionData, _) {
+    return await this.adyenService_.updatePayment(paymentSessionData)
+  }
+
+  async updatePaymentData(paymentSessionData, data) {
+    return await this.adyenService_.updatePaymentData(paymentSessionData, data)
+  }
+
+  async getPaymentData(paymentSession) {
+    return await this.adyenService_.getPaymentData(paymentSession)
+  }
+
+  async deletePayment(paymentSession) {
+    return await this.adyenService_.deletePayment(paymentSession)
+  }
+
+  async capturePayment(payment) {
+    return await this.adyenService_.capturePayment(payment)
+  }
+
+  async refundPayment(payment, amountToRefund) {
+    return await this.adyenService_.refundPayment(payment, amountToRefund)
+  }
+
+  async cancelPayment(payment) {
+    return await this.adyenService_.cancelPayment(payment)
+  }
+}
+
+export default MobilePayAdyenService
---
+++ packages/medusa/src/interfaces/index.ts
@@ -0,0 +1,5 @@
+export * from "./tax-calculation-strategy"
+export * from "./cart-completion-strategy"
+export * from "./tax-service"
+export * from "./transaction-base-service"
+export * from "./payment-service"
