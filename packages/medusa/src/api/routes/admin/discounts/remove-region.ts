94e7a5671fa8066c0af1b97cec73cda862832684
Sammy filly <136061549+sammyfilly@users.noreply.github.com>
2023-09-11 22:53:34 +0100

    Initialize fix/bp-snapshot

---
+++ packages/medusa/src/api/routes/admin/discounts/remove-region.ts
@@ -0,0 +1,75 @@
+import { defaultAdminDiscountsFields, defaultAdminDiscountsRelations } from "."
+
+import DiscountService from "../../../../services/discount"
+import { EntityManager } from "typeorm"
+
+/**
+ * @oas [delete] /admin/discounts/{id}/regions/{region_id}
+ * operationId: "DeleteDiscountsDiscountRegionsRegion"
+ * summary: "Remove Region"
+ * x-authenticated: true
+ * description: "Remove a Region from the list of Regions that a Discount can be used in. This does not delete a region, only the association between it and the discount."
+ * parameters:
+ *   - (path) id=* {string} The ID of the Discount.
+ *   - (path) region_id=* {string} The ID of the Region.
+ * x-codegen:
+ *   method: removeRegion
+ * x-codeSamples:
+ *   - lang: JavaScript
+ *     label: JS Client
+ *     source: |
+ *       import Medusa from "@medusajs/medusa-js"
+ *       const medusa = new Medusa({ baseUrl: MEDUSA_BACKEND_URL, maxRetries: 3 })
+ *       // must be previously logged in or use api token
+ *       medusa.admin.discounts.removeRegion(discountId, regionId)
+ *       .then(({ discount }) => {
+ *         console.log(discount.id);
+ *       });
+ *   - lang: Shell
+ *     label: cURL
+ *     source: |
+ *       curl -X DELETE '{backend_url}/admin/discounts/{id}/regions/{region_id}' \
+ *       -H 'Authorization: Bearer {api_token}'
+ * security:
+ *   - api_token: []
+ *   - cookie_auth: []
+ * tags:
+ *   - Discounts
+ * responses:
+ *   200:
+ *     description: OK
+ *     content:
+ *       application/json:
+ *         schema:
+ *           $ref: "#/components/schemas/AdminDiscountsRes"
+ *   "400":
+ *     $ref: "#/components/responses/400_error"
+ *   "401":
+ *     $ref: "#/components/responses/unauthorized"
+ *   "404":
+ *     $ref: "#/components/responses/not_found_error"
+ *   "409":
+ *     $ref: "#/components/responses/invalid_state_error"
+ *   "422":
+ *     $ref: "#/components/responses/invalid_request_error"
+ *   "500":
+ *     $ref: "#/components/responses/500_error"
+ */
+export default async (req, res) => {
+  const { discount_id, region_id } = req.params
+
+  const discountService: DiscountService = req.scope.resolve("discountService")
+  const manager: EntityManager = req.scope.resolve("manager")
+  await manager.transaction(async (transactionManager) => {
+    return await discountService
+      .withTransaction(transactionManager)
+      .removeRegion(discount_id, region_id)
+  })
+
+  const discount = await discountService.retrieve(discount_id, {
+    select: defaultAdminDiscountsFields,
+    relations: defaultAdminDiscountsRelations,
+  })
+
+  res.status(200).json({ discount })
+}
