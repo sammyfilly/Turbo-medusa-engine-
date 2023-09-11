049d185f26307b5473e33990eb42787315ff22a5
Sammy filly <136061549+sammyfilly@users.noreply.github.com>
2023-09-11 22:11:45 +0100

    Initialize feat/create-fulfillment

---
+++ packages/medusa/src/commands/migrate.js
@@ -0,0 +1,57 @@
+import { asValue, createContainer } from "awilix"
+import featureFlagLoader from "../loaders/feature-flags"
+import Logger from "../loaders/logger"
+import databaseLoader from "../loaders/database"
+import configModuleLoader from "../loaders/config"
+import getMigrations, { getModuleSharedResources } from "./utils/get-migrations"
+
+const getDataSource = async (directory) => {
+  const configModule = configModuleLoader(directory)
+  const featureFlagRouter = featureFlagLoader(configModule)
+  const { coreMigrations } = getMigrations(directory, featureFlagRouter)
+  const { migrations: moduleMigrations } = getModuleSharedResources(
+    configModule,
+    featureFlagRouter
+  )
+
+  const container = createContainer()
+  container.register("db_entities", asValue([]))
+
+  return await databaseLoader({
+    container,
+    configModule,
+    customOptions: {
+      migrations: coreMigrations.concat(moduleMigrations),
+      logging: "all",
+    },
+  })
+}
+
+const main = async function ({ directory }) {
+  const args = process.argv
+
+  args.shift()
+  args.shift()
+  args.shift()
+
+  const dataSource = await getDataSource(directory)
+
+  if (args[0] === "run") {
+    await dataSource.runMigrations()
+    await dataSource.destroy()
+    Logger.info("Migrations completed.")
+    process.exit()
+  } else if (args[0] === "revert") {
+    await dataSource.undoLastMigration({ transaction: "all" })
+    await dataSource.destroy()
+    Logger.info("Migrations reverted.")
+    process.exit()
+  } else if (args[0] === "show") {
+    const unapplied = await dataSource.showMigrations()
+    Logger.info(unapplied)
+    await dataSource.destroy()
+    process.exit(unapplied ? 1 : 0)
+  }
+}
+
+export default main
