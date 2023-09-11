ebe011a4b7b82e3bc4de020f5254d5d9bbc73137
Sammy filly <136061549+sammyfilly@users.noreply.github.com>
2023-09-11 21:35:22 +0100

    Initialize chore/transaction-crud-service-basics

---
+++ integration-tests/api/jest.config.js
@@ -0,0 +1,23 @@
+// API
+process.chdir(__dirname)
+
+module.exports = {
+  name: "api",
+  testEnvironment: `node`,
+  rootDir: "./",
+  testTimeout: 10000,
+  testPathIgnorePatterns: [
+    `/examples/`,
+    `/www/`,
+    `/dist/`,
+    `/node_modules/`,
+    `__tests__/fixtures`,
+    `__testfixtures__`,
+    `.cache`,
+  ],
+  transformIgnorePatterns: [`/dist`],
+  transform: { "^.+\\.[jt]s$": `../../jest-transformer.js` },
+  setupFilesAfterEnv: ["../setup.js"],
+  globalSetup: "../globalSetup.js",
+  globalTeardown: "../globalTeardown.js",
+}
