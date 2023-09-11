1fdd1530d53b548b005ff9658c7a1f449edca11b
Sammy filly <136061549+sammyfilly@users.noreply.github.com>
2023-09-11 22:08:06 +0100

    Initialize feat/allowed-props-admin-orders

---
+++ packages/medusa/src/api/middlewares/transform-query.ts
@@ -0,0 +1,73 @@
+import { ValidatorOptions } from "class-validator"
+import { NextFunction, Request, Response } from "express"
+import { omit } from "lodash"
+import { BaseEntity } from "../../interfaces"
+import { FindConfig, QueryConfig, RequestQueryFields } from "../../types/common"
+import { ClassConstructor } from "../../types/global"
+import { removeUndefinedProperties } from "../../utils"
+import {
+  prepareListQuery,
+  prepareRetrieveQuery,
+} from "../../utils/get-query-config"
+import { validator } from "../../utils/validator"
+import { default as normalizeQuery } from "./normalized-query"
+
+export function transformQuery<
+  T extends RequestQueryFields,
+  TEntity extends BaseEntity
+>(
+  plainToClass: ClassConstructor<T>,
+  queryConfig?: QueryConfig<TEntity>,
+  config: ValidatorOptions = {}
+): (req: Request, res: Response, next: NextFunction) => Promise<void> {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      normalizeQuery()(req, res, () => void 0)
+      const validated: T = await validator<T, Record<string, unknown>>(
+        plainToClass,
+        req.query,
+        config
+      )
+      req.validatedQuery = validated
+
+      req.filterableFields = omit(validated, [
+        "limit",
+        "offset",
+        "expand",
+        "fields",
+        "order",
+      ])
+      req.filterableFields = removeUndefinedProperties(req.filterableFields)
+
+      if (
+        (queryConfig?.defaultFields || validated.fields) &&
+        (queryConfig?.defaultRelations || validated.expand)
+      ) {
+        req.allowedProperties = [
+          ...(validated.fields
+            ? validated.fields.split(",")
+            : queryConfig?.allowedFields || [])!,
+          ...(validated.expand
+            ? validated.expand.split(",")
+            : queryConfig?.allowedRelations || [])!,
+        ] as unknown as string[]
+      }
+
+      if (queryConfig?.isList) {
+        req.listConfig = prepareListQuery(
+          validated,
+          queryConfig
+        ) as FindConfig<unknown>
+      } else {
+        req.retrieveConfig = prepareRetrieveQuery(
+          validated,
+          queryConfig
+        ) as FindConfig<unknown>
+      }
+
+      next()
+    } catch (e) {
+      next(e)
+    }
+  }
+}
