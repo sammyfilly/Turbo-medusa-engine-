eff9d52bd8d768df330f151c4931ac0f9abd37dc
Sammy filly <136061549+sammyfilly@users.noreply.github.com>
2023-09-11 21:32:18 +0100

    Initialize chore/medusa-js-deps

---
+++ packages/medusa/src/api/routes/admin/products/list-products.ts
@@ -0,0 +1,184 @@
+import { Transform, Type } from "class-transformer"
+import {
+  IsArray,
+  IsBoolean,
+  IsEnum,
+  IsNumber,
+  IsOptional,
+  IsString,
+  ValidateNested,
+} from "class-validator"
+import { omit } from "lodash"
+import { Product, ProductStatus } from "../../../../models/product"
+import { DateComparisonOperator } from "../../../../types/common"
+import {
+  allowedAdminProductFields,
+  defaultAdminProductFields,
+  defaultAdminProductRelations,
+} from "."
+import listAndCount from "../../../../controllers/products/admin-list-products"
+import { validator } from "../../../../utils/validator"
+import { optionalBooleanMapper } from "../../../../utils/validators/is-boolean"
+
+/**
+ * @oas [get] /products
+ * operationId: "GetProducts"
+ * summary: "List Product"
+ * description: "Retrieves a list of Product"
+ * x-authenticated: true
+ * parameters:
+ *   - (query) q {string} Query used for searching products.
+ *   - (query) id {string} Id of the product to search for.
+ *   - (query) status {string[]} Status to search for.
+ *   - (query) collection_id {string[]} Collection ids to search for.
+ *   - (query) tags {string[]} Tags to search for.
+ *   - (query) title {string} to search for.
+ *   - (query) description {string} to search for.
+ *   - (query) handle {string} to search for.
+ *   - (query) is_giftcard {string} Search for giftcards using is_giftcard=true.
+ *   - (query) type {string} to search for.
+ *   - (query) order {string} to retrieve products in.
+ *   - (query) deleted_at {DateComparisonOperator} Date comparison for when resulting products was deleted, i.e. less than, greater than etc.
+ *   - (query) created_at {DateComparisonOperator} Date comparison for when resulting products was created, i.e. less than, greater than etc.
+ *   - (query) updated_at {DateComparisonOperator} Date comparison for when resulting products was updated, i.e. less than, greater than etc.
+ *   - (query) offset {string} How many products to skip in the result.
+ *   - (query) limit {string} Limit the number of products returned.
+ *   - (query) expand {string} (Comma separated) Which fields should be expanded in each product of the result.
+ *   - (query) fields {string} (Comma separated) Which fields should be included in each product of the result.
+ * tags:
+ *   - Product
+ * responses:
+ *   200:
+ *     description: OK
+ *     content:
+ *       application/json:
+ *         schema:
+ *           properties:
+ *             count:
+ *               description: The number of Products.
+ *               type: integer
+ *             offset:
+ *               description: The offset of the Product query.
+ *               type: integer
+ *             limit:
+ *               description: The limit of the Product query.
+ *               type: integer
+ *             products:
+ *               type: array
+ *               items:
+ *                 $ref: "#/components/schemas/product"
+ */
+export default async (req, res) => {
+  const validatedParams = await validator(AdminGetProductsParams, req.query)
+
+  const filterableFields = omit(validatedParams, [
+    "limit",
+    "offset",
+    "expand",
+    "fields",
+    "order",
+  ])
+
+  const result = await listAndCount(
+    req.scope,
+    filterableFields,
+    {},
+    {
+      limit: validatedParams.limit ?? 50,
+      offset: validatedParams.offset ?? 0,
+      expand: validatedParams.expand,
+      fields: validatedParams.fields,
+      allowedFields: allowedAdminProductFields,
+      defaultFields: defaultAdminProductFields as (keyof Product)[],
+      defaultRelations: defaultAdminProductRelations,
+    }
+  )
+
+  res.json(result)
+}
+
+export class AdminGetProductsPaginationParams {
+  @IsNumber()
+  @IsOptional()
+  @Type(() => Number)
+  offset?: number = 0
+
+  @IsNumber()
+  @IsOptional()
+  @Type(() => Number)
+  limit?: number = 50
+
+  @IsString()
+  @IsOptional()
+  expand?: string
+
+  @IsString()
+  @IsOptional()
+  fields?: string
+}
+
+export class AdminGetProductsParams extends AdminGetProductsPaginationParams {
+  @IsString()
+  @IsOptional()
+  id?: string
+
+  @IsString()
+  @IsOptional()
+  q?: string
+
+  @IsOptional()
+  @IsEnum(ProductStatus, { each: true })
+  status?: ProductStatus[]
+
+  @IsArray()
+  @IsOptional()
+  collection_id?: string[]
+
+  @IsArray()
+  @IsOptional()
+  tags?: string[]
+
+  @IsArray()
+  @IsOptional()
+  price_list_id?: string[]
+
+  @IsString()
+  @IsOptional()
+  title?: string
+
+  @IsString()
+  @IsOptional()
+  description?: string
+
+  @IsString()
+  @IsOptional()
+  handle?: string
+
+  @IsBoolean()
+  @IsOptional()
+  @Transform(({ value }) => optionalBooleanMapper.get(value.toLowerCase()))
+  is_giftcard?: boolean
+
+  @IsString()
+  @IsOptional()
+  type?: string
+
+  @IsString()
+  @IsOptional()
+  order?: string
+
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => DateComparisonOperator)
+  created_at?: DateComparisonOperator
+
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => DateComparisonOperator)
+  updated_at?: DateComparisonOperator
+
+  @ValidateNested()
+  @IsOptional()
+  @Type(() => DateComparisonOperator)
+  deleted_at?: DateComparisonOperator
+}
