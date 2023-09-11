2367ca584c2444444833c1d41d26c57b0c5fb5b5
Sammy filly <136061549+sammyfilly@users.noreply.github.com>
2023-09-11 21:28:32 +0100

    Initialize fix/add-shipping-method

---
+++ .eslintrc.js
@@ -0,0 +1,53 @@
+module.exports = {
+  parser: `@babel/eslint-parser`,
+  parserOptions: {
+    requireConfigFile: false,
+    ecmaFeatures: {
+      experimentalDecorators: true,
+    },
+  },
+  plugins: [`eslint-plugin-prettier`],
+  extends: [`eslint:recommended`, `google`, `eslint-config-prettier`],
+  rules: {
+    "prettier/prettier": `error`,
+    curly: [2, `all`],
+    "new-cap": `off`,
+    "require-jsdoc": `off`,
+    semi: `off`,
+    "no-unused-expressions": `off`,
+    camelcase: `off`,
+    "no-invalid-this": `off`,
+  },
+  env: {
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: [`**/models`, `**/repositories`],
+  overrides: [
+    {
+      files: [`*.ts`],
+      parser: `@typescript-eslint/parser`,
+      plugins: [`@typescript-eslint/eslint-plugin`],
+      extends: [`plugin:@typescript-eslint/recommended`],
+      rules: {
+        "@typescript-eslint/explicit-function-return-type": ["error"],
+        "@typescript-eslint/no-non-null-assertion": ["off"],
+      },
+    },
+    {
+      files: ["**/api/**/*.js", "**/api/**/*.ts"],
+      rules: {
+        "valid-jsdoc": ["off"],
+      },
+    },
+    {
+      files: ["**/api/**/*.ts"],
+      rules: {
+        "valid-jsdoc": ["off"],
+        "@typescript-eslint/explicit-function-return-type": ["off"],
+        "@typescript-eslint/no-var-requires": ["off"],
+      },
+    },
+  ],
+}
