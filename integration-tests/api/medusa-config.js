7a913668f78c5c241a1924d752f23ef634550384
Sammy filly <136061549+sammyfilly@users.noreply.github.com>
2023-09-11 21:27:59 +0100

    Initialize fix/settings-after-walkthrough

---
+++ integration-tests/api/medusa-config.js
@@ -0,0 +1,11 @@
+const DB_USERNAME = process.env.DB_USERNAME || "postgres"
+const DB_PASSWORD = process.env.DB_PASSWORD || ""
+
+module.exports = {
+  plugins: [],
+  projectConfig: {
+    // redis_url: REDIS_URL,
+    database_url: `postgres://${DB_USERNAME}:${DB_PASSWORD}@localhost/medusa-integration`,
+    database_type: "postgres",
+  },
+}
