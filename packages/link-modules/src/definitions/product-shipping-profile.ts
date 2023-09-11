dd35534584cd96b1c9fee8666b0ec61b9d4f3996
Sammy filly <136061549+sammyfilly@users.noreply.github.com>
2023-09-11 22:55:29 +0100

    Initialize feat/price-sets

---
+++ packages/link-modules/src/definitions/product-shipping-profile.ts
@@ -0,0 +1,55 @@
+import { Modules } from "@medusajs/modules-sdk"
+import { ModuleJoinerConfig } from "@medusajs/types"
+import { LINKS } from "../links"
+
+export const ProductShippingProfile: ModuleJoinerConfig = {
+  serviceName: LINKS.ProductShippingProfile,
+  isLink: true,
+  databaseConfig: {
+    tableName: "product_shipping_profile",
+    idPrefix: "psprof",
+  },
+  alias: [
+    {
+      name: "product_shipping_profile",
+    },
+  ],
+  primaryKeys: ["id", "product_id", "profile_id"],
+  relationships: [
+    {
+      serviceName: Modules.PRODUCT,
+      primaryKey: "id",
+      foreignKey: "product_id",
+      alias: "product",
+    },
+    {
+      serviceName: "shippingProfileService",
+      isInternalService: true,
+      primaryKey: "id",
+      foreignKey: "profile_id",
+      alias: "profile",
+    },
+  ],
+  extends: [
+    {
+      serviceName: Modules.PRODUCT,
+      relationship: {
+        serviceName: LINKS.ProductShippingProfile,
+        isInternalService: true,
+        primaryKey: "product_id",
+        foreignKey: "id",
+        alias: "shipping_profile",
+      },
+    },
+    {
+      serviceName: "shippingProfileService",
+      relationship: {
+        serviceName: LINKS.ProductShippingProfile,
+        isInternalService: true,
+        primaryKey: "profile_id",
+        foreignKey: "id",
+        alias: "product_link",
+      },
+    },
+  ],
+}
