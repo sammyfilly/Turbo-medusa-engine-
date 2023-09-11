f16e08622623e203daedae9207ff8515d777a2db
Sammy filly <136061549+sammyfilly@users.noreply.github.com>
2023-09-11 21:35:51 +0100

    Initialize chore/oas-request-examples

---
+++ packages/medusa/src/api/routes/store/products/get-product.ts
@@ -0,0 +1,92 @@
+import {
+  CartService,
+  PricingService,
+  ProductService,
+  RegionService,
+} from "../../../../services"
+
+import { PriceSelectionParams } from "../../../../types/price-selection"
+import { defaultStoreProductsRelations } from "."
+import { validator } from "../../../../utils/validator"
+
+/**
+ * @oas [get] /products/{id}
+ * operationId: GetProductsProduct
+ * summary: Retrieves a Product
+ * description: "Retrieves a Product."
+ * parameters:
+ *   - (path) id=* {string} The id of the Product.
+ * x-codeSamples:
+ *   - lang: JavaScript
+ *     label: JS Client
+ *     source: |
+ *       import Medusa from "@medusajs/medusa-js"
+ *       const medusa = new Medusa({ baseUrl: MEDUSA_BACKEND_URL, maxRetries: 3 })
+ *       medusa.products.retrieve(product_id)
+ *       .then(({ product }) => {
+ *         console.log(product.id);
+ *       });
+ *   - lang: Shell
+ *     label: cURL
+ *     source: |
+ *       curl --location --request GET 'https://medusa-url.com/store/products/{id}'
+ * tags:
+ *   - Product
+ * responses:
+ *   200:
+ *     description: OK
+ *     content:
+ *       application/json:
+ *         schema:
+ *           properties:
+ *             product:
+ *               $ref: "#/components/schemas/product"
+ *   "400":
+ *     $ref: "#/components/responses/400_error"
+ *   "404":
+ *     $ref: "#/components/responses/not_found_error"
+ *   "409":
+ *     $ref: "#/components/responses/invalid_state_error"
+ *   "422":
+ *     $ref: "#/components/responses/invalid_request_error"
+ *   "500":
+ *     $ref: "#/components/responses/500_error"
+ */
+export default async (req, res) => {
+  const { id } = req.params
+
+  const validated = await validator(PriceSelectionParams, req.query)
+
+  const customer_id = req.user?.customer_id
+
+  const productService: ProductService = req.scope.resolve("productService")
+  const pricingService: PricingService = req.scope.resolve("pricingService")
+  const cartService: CartService = req.scope.resolve("cartService")
+  const regionService: RegionService = req.scope.resolve("regionService")
+  const rawProduct = await productService.retrieve(id, {
+    relations: defaultStoreProductsRelations,
+  })
+
+  let regionId = validated.region_id
+  let currencyCode = validated.currency_code
+  if (validated.cart_id) {
+    const cart = await cartService.retrieve(validated.cart_id, {
+      select: ["id", "region_id"],
+    })
+    const region = await regionService.retrieve(cart.region_id, {
+      select: ["id", "currency_code"],
+    })
+    regionId = region.id
+    currencyCode = region.currency_code
+  }
+
+  const [product] = await pricingService.setProductPrices([rawProduct], {
+    cart_id: validated.cart_id,
+    customer_id: customer_id,
+    region_id: regionId,
+    currency_code: currencyCode,
+    include_discount_prices: true,
+  })
+
+  res.json({ product })
+}
