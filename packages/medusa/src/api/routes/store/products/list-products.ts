d090ce2707766c60c2b6764b1909f841449e41c9
Sammy filly <136061549+sammyfilly@users.noreply.github.com>
2023-09-11 22:55:00 +0100

    Initialize feat/list-produt-isolated-module

---
+++ packages/medusa/src/api/routes/store/products/list-products.ts
@@ -0,0 +1,580 @@
+import {
+  CartService,
+  ProductService,
+  ProductVariantInventoryService,
+  SalesChannelService,
+} from "../../../../services"
+import {
+  IsArray,
+  IsBoolean,
+  IsNumber,
+  IsOptional,
+  IsString,
+  ValidateNested,
+} from "class-validator"
+import { Transform, Type } from "class-transformer"
+
+import { DateComparisonOperator } from "../../../../types/common"
+import { FeatureFlagDecorators } from "../../../../utils/feature-flag-decorators"
+import { IsType } from "../../../../utils/validators/is-type"
+import { PriceSelectionParams } from "../../../../types/price-selection"
+import PricingService from "../../../../services/pricing"
+import SalesChannelFeatureFlag from "../../../../loaders/feature-flags/sales-channels"
+import { cleanResponseData } from "../../../../utils/clean-response-data"
+import { defaultStoreCategoryScope } from "../product-categories"
+import { optionalBooleanMapper } from "../../../../utils/validators/is-boolean"
+import IsolateProductDomain from "../../../../loaders/feature-flags/isolate-product-domain"
+import { defaultStoreProductsFields } from "./index"
+
+/**
+ * @oas [get] /store/products
+ * operationId: GetProducts
+ * summary: List Products
+ * description: |
+ *   Retrieves a list of products. The products can be filtered by fields such as `id` or `q`. The products can also be sorted or paginated.
+ *   This endpoint can also be used to retrieve a product by its handle.
+ *
+ *   For accurate and correct pricing of the products based on the customer's context, it's highly recommended to pass fields such as
+ *   `region_id`, `currency_code`, and `cart_id` when available.
+ *
+ *   Passing `sales_channel_id` ensures retrieving only products available in the specified sales channel.
+ *   You can alternatively use a publishable API key in the request header instead of passing a `sales_channel_id`.
+ * externalDocs:
+ *   description: "How to retrieve a product by its handle"
+ *   url: "https://docs.medusajs.com/modules/products/storefront/show-products#retrieve-product-by-handle"
+ * parameters:
+ *   - (query) q {string} term used to search products' title, description, variant's title, variant's sku, and collection's title.
+ *   - in: query
+ *     name: id
+ *     style: form
+ *     explode: false
+ *     description: Filter by IDs.
+ *     schema:
+ *       oneOf:
+ *         - type: string
+ *         - type: array
+ *           items:
+ *             type: string
+ *   - in: query
+ *     name: sales_channel_id
+ *     style: form
+ *     explode: false
+ *     description: "Filter by sales channel IDs. When provided, only products available in the selected sales channels are retrieved. Alternatively, you can pass a
+ *      publishable API key in the request header and this will have the same effect."
+ *     schema:
+ *       type: array
+ *       items:
+ *         type: string
+ *   - in: query
+ *     name: collection_id
+ *     style: form
+ *     explode: false
+ *     description: Filter by product collection IDs. When provided, only products that belong to the specified product collections are retrieved.
+ *     schema:
+ *       type: array
+ *       items:
+ *         type: string
+ *   - in: query
+ *     name: type_id
+ *     style: form
+ *     explode: false
+ *     description: Filter by product type IDs. When provided, only products that belong to the specified product types are retrieved.
+ *     schema:
+ *       type: array
+ *       items:
+ *         type: string
+ *   - in: query
+ *     name: tags
+ *     style: form
+ *     explode: false
+ *     description: Filter by product tag IDs. When provided, only products that belong to the specified product tags are retrieved.
+ *     schema:
+ *       type: array
+ *       items:
+ *         type: string
+ *   - (query) title {string} Filter by title.
+ *   - (query) description {string} Filter by description
+ *   - (query) handle {string} Filter by handle.
+ *   - (query) is_giftcard {boolean} Whether to retrieve regular products or gift-card products.
+ *   - in: query
+ *     name: created_at
+ *     description: Filter by a creation date range.
+ *     schema:
+ *       type: object
+ *       properties:
+ *         lt:
+ *            type: string
+ *            description: filter by dates less than this date
+ *            format: date
+ *         gt:
+ *            type: string
+ *            description: filter by dates greater than this date
+ *            format: date
+ *         lte:
+ *            type: string
+ *            description: filter by dates less than or equal to this date
+ *            format: date
+ *         gte:
+ *            type: string
+ *            description: filter by dates greater than or equal to this date
+ *            format: date
+ *   - in: query
+ *     name: updated_at
+ *     description: Filter by an update date range.
+ *     schema:
+ *       type: object
+ *       properties:
+ *         lt:
+ *            type: string
+ *            description: filter by dates less than this date
+ *            format: date
+ *         gt:
+ *            type: string
+ *            description: filter by dates greater than this date
+ *            format: date
+ *         lte:
+ *            type: string
+ *            description: filter by dates less than or equal to this date
+ *            format: date
+ *         gte:
+ *            type: string
+ *            description: filter by dates greater than or equal to this date
+ *            format: date
+ *   - in: query
+ *     name: category_id
+ *     style: form
+ *     explode: false
+ *     description: Filter by product category IDs. When provided, only products that belong to the specified product categories are retrieved.
+ *     schema:
+ *       type: array
+ *       x-featureFlag: "product_categories"
+ *       items:
+ *         type: string
+ *   - in: query
+ *     name: include_category_children
+ *     style: form
+ *     explode: false
+ *     description: Whether to include child product categories when filtering using the `category_id` field.
+ *     schema:
+ *       type: boolean
+ *       x-featureFlag: "product_categories"
+ *   - (query) offset=0 {integer} The number of products to skip when retrieving the products.
+ *   - (query) limit=100 {integer} Limit the number of products returned.
+ *   - (query) expand {string} Comma-separated relations that should be expanded in the returned products.
+ *   - (query) fields {string} Comma-separated fields that should be included in the returned products.
+ *   - (query) order {string} A product field to sort-order the retrieved products by.
+ *   - (query) cart_id {string} The ID of the cart. This is useful for accurate pricing based on the cart's context.
+ *   - (query) region_id {string} The ID of the region. This is useful for accurate pricing based on the selected region.
+ *   - in: query
+ *     name: currency_code
+ *     style: form
+ *     explode: false
+ *     description: A 3 character ISO currency code. This is useful for accurate pricing based on the selected currency.
+ *     schema:
+ *       type: string
+ *       externalDocs:
+ *         url: https://en.wikipedia.org/wiki/ISO_4217#Active_codes
+ *         description: See a list of codes.
+ * x-codegen:
+ *   method: list
+ *   queryParams: StoreGetProductsParams
+ * x-codeSamples:
+ *   - lang: JavaScript
+ *     label: JS Client
+ *     source: |
+ *       import Medusa from "@medusajs/medusa-js"
+ *       const medusa = new Medusa({ baseUrl: MEDUSA_BACKEND_URL, maxRetries: 3 })
+ *       medusa.products.list()
+ *       .then(({ products, limit, offset, count }) => {
+ *         console.log(products.length);
+ *       });
+ *   - lang: Shell
+ *     label: cURL
+ *     source: |
+ *       curl '{backend_url}/store/products'
+ * tags:
+ *   - Products
+ * responses:
+ *   200:
+ *     description: OK
+ *     content:
+ *       application/json:
+ *         schema:
+ *           $ref: "#/components/schemas/StoreProductsListRes"
+ *   "400":
+ *     $ref: "#/components/responses/400_error"
+ *   "404":
+ *     $ref: "#/components/responses/not_found_error"
+ *   "409":
+ *     $ref: "#/components/responses/invalid_state_error"
+ *   "422":
+ *     $ref: "#/components/responses/invalid_request_error"
+ *   "500":
+ *     $ref: "#/components/responses/500_error"
+ */
+export default async (req, res) => {
+  const productService: ProductService = req.scope.resolve("productService")
+  const productVariantInventoryService: ProductVariantInventoryService =
+    req.scope.resolve("productVariantInventoryService")
+  const pricingService: PricingService = req.scope.resolve("pricingService")
+  const cartService: CartService = req.scope.resolve("cartService")
+
+  const featureFlagRouter = req.scope.resolve("featureFlagRouter")
+
+  const validated = req.validatedQuery as StoreGetProductsParams
+
+  let {
+    cart_id,
+    region_id: regionId,
+    currency_code: currencyCode,
+    ...filterableFields
+  } = req.filterableFields
+  const listConfig = req.listConfig
+
+  // get only published products for store endpoint
+  filterableFields["status"] = ["published"]
+  // store APIs only receive active and public categories to query from
+  filterableFields["categories"] = {
+    ...(filterableFields.categories || {}),
+    // Store APIs are only allowed to query active and public categories
+    ...defaultStoreCategoryScope,
+  }
+
+  if (req.publishableApiKeyScopes?.sales_channel_ids.length) {
+    filterableFields.sales_channel_id =
+      filterableFields.sales_channel_id ||
+      req.publishableApiKeyScopes.sales_channel_ids
+
+    if (!listConfig.relations.includes("listConfig.relations")) {
+      listConfig.relations.push("sales_channels")
+    }
+  }
+
+  const isIsolateProductDomain = featureFlagRouter.isFeatureEnabled(
+    IsolateProductDomain.key
+  )
+
+  const promises: Promise<any>[] = []
+
+  if (isIsolateProductDomain) {
+    promises.push(
+      listAndCountProductWithIsolatedProductModule(
+        req,
+        filterableFields,
+        listConfig
+      )
+    )
+  } else {
+    promises.push(productService.listAndCount(filterableFields, listConfig))
+  }
+
+  if (validated.cart_id) {
+    promises.push(
+      cartService.retrieve(validated.cart_id, {
+        select: ["id", "region_id"] as any,
+        relations: ["region"],
+      })
+    )
+  }
+
+  const [[rawProducts, count], cart] = await Promise.all(promises)
+
+  if (validated.cart_id) {
+    regionId = cart.region_id
+    currencyCode = cart.region.currency_code
+  }
+
+  // Create a new reference just for naming purpose
+  const computedProducts = rawProducts
+
+  // We only set prices if variants.prices are requested
+  const shouldSetPricing = ["variants", "variants.prices"].every((relation) =>
+    listConfig.relations?.includes(relation)
+  )
+
+  // We only set availability if variants are requested
+  const shouldSetAvailability = listConfig.relations?.includes("variants")
+
+  const decoratePromises: Promise<any>[] = []
+
+  if (shouldSetPricing) {
+    decoratePromises.push(
+      pricingService.setProductPrices(computedProducts, {
+        cart_id: cart_id,
+        region_id: regionId,
+        currency_code: currencyCode,
+        customer_id: req.user?.customer_id,
+        include_discount_prices: true,
+      })
+    )
+  }
+
+  if (shouldSetAvailability) {
+    decoratePromises.push(
+      productVariantInventoryService.setProductAvailability(
+        computedProducts,
+        filterableFields.sales_channel_id
+      )
+    )
+  }
+
+  // We can run them concurrently as the new properties are assigned to the references
+  // of the appropriate entity
+  await Promise.all(decoratePromises)
+
+  res.json({
+    products: cleanResponseData(computedProducts, req.allowedProperties || []),
+    count,
+    offset: validated.offset,
+    limit: validated.limit,
+  })
+}
+
+async function listAndCountProductWithIsolatedProductModule(
+  req,
+  filterableFields,
+  listConfig
+) {
+  // TODO: Add support for fields/expands
+
+  const remoteQuery = req.scope.resolve("remoteQuery")
+
+  let salesChannelIdFilter = filterableFields.sales_channel_id
+  if (req.publishableApiKeyScopes?.sales_channel_ids.length) {
+    salesChannelIdFilter ??= req.publishableApiKeyScopes.sales_channel_ids
+  }
+
+  delete filterableFields.sales_channel_id
+
+  filterableFields["categories"] = {
+    $or: [
+      {
+        id: null,
+      },
+      {
+        ...(filterableFields.categories || {}),
+        // Store APIs are only allowed to query active and public categories
+        ...defaultStoreCategoryScope,
+      },
+    ],
+  }
+
+  // This is not the best way of handling cross filtering but for now I would say it is fine
+  if (salesChannelIdFilter) {
+    const salesChannelService = req.scope.resolve(
+      "salesChannelService"
+    ) as SalesChannelService
+
+    const productIdsInSalesChannel =
+      await salesChannelService.listProductIdsBySalesChannelIds(
+        salesChannelIdFilter
+      )
+
+    let filteredProductIds = productIdsInSalesChannel[salesChannelIdFilter]
+
+    if (filterableFields.id) {
+      filterableFields.id = Array.isArray(filterableFields.id)
+        ? filterableFields.id
+        : [filterableFields.id]
+
+      const salesChannelProductIdsSet = new Set(filteredProductIds)
+
+      filteredProductIds = filterableFields.id.filter((productId) =>
+        salesChannelProductIdsSet.has(productId)
+      )
+    }
+
+    filterableFields.id = filteredProductIds
+  }
+
+  const variables = {
+    filters: filterableFields,
+    order: listConfig.order,
+    skip: listConfig.skip,
+    take: listConfig.take,
+  }
+
+  // prettier-ignore
+  const args = `
+    filters: $filters,
+    order: $order,
+    skip: $skip, 
+    take: $take
+  `
+
+  const commonProperties = ["id", "created_at", "updated_at", "deleted_at"]
+
+  const query = `
+      query ($filters: any, $order: any, $skip: Int, $take: Int) {
+        product (${args}) {
+          ${defaultStoreProductsFields.join("\n")}
+          
+          images {
+            ${commonProperties.join("\n")}
+            url
+            metadata
+          }
+          
+          tags {
+            ${commonProperties.join("\n")}
+            value
+          }
+          
+          type {
+            ${commonProperties.join("\n")}
+            value
+          }
+          
+          collection {
+            title
+            handle
+            ${commonProperties.join("\n")}
+          }
+          
+          options {
+            ${commonProperties.join("\n")}
+            title
+            product_id
+            metadata
+            values {
+              ${commonProperties.join("\n")}
+              value
+              option_id
+              variant_id
+              metadata
+            }
+          }
+          
+          variants {
+            ${commonProperties.join("\n")}
+            title
+            product_id
+            sku
+            barcode
+            ean
+            upc
+            variant_rank
+            inventory_quantity
+            allow_backorder
+            manage_inventory
+            hs_code
+            origin_country
+            mid_code
+            material
+            weight
+            length
+            height
+            width
+            metadata
+            options {
+              ${commonProperties.join("\n")}
+              value
+              option_id
+              variant_id
+              metadata
+            }
+          }
+          
+          shipping_profile {
+            profile {
+              id
+              name
+              type
+            }
+          }
+        } 
+      }
+    `
+
+  const {
+    rows: products,
+    metadata: { count },
+  } = await remoteQuery(query, variables)
+
+  products.forEach((product) => {
+    product.profile_id = product.shipping_profile?.profile?.id
+    product.profiles = product.shipping_profile?.profile
+      ? [product.shipping_profile?.profile]
+      : undefined
+    product.profile = product.shipping_profile?.profile
+  })
+
+  return [products, count]
+}
+
+export class StoreGetProductsPaginationParams extends PriceSelectionParams {
+  @IsNumber()
+  @IsOptional()
+  @Type(() => Number)
+  offset?: number = 0
+
+  @IsNumber()
+  @IsOptional()
+  @Type(() => Number)
+  limit?: number = 100
+
+  @IsString()
+  @IsOptional()
+  order?: string
+}
+
+export class StoreGetProductsParams extends StoreGetProductsPaginationParams {
+  @IsOptional()
+  @IsType([String, [String]])
+  id?: string | string[]
+
+  @IsString()
+  @IsOptional()
+  q?: string
+
+  @IsArray()
+  @IsOptional()
+  collection_id?: string[]
+
+  @IsArray()
+  @IsOptional()
+  tags?: string[]
+
+  @IsString()
+  @IsOptional()
+  title?: string
+
+  @IsString()
+  @IsOptional()
+  description?: string
+
+  @IsString()
+  @IsOptional()
+  handle?: string
+
+  @IsBoolean()
+  @IsOptional()
+  @Transform(({ value }) => optionalBooleanMapper.get(value.toLowerCase()))
+  is_giftcard?: boolean
+
+  @IsArray()
+  @IsOptional()
+  type_id?: string[]
+
+  @FeatureFlagDecorators(SalesChannelFeatureFlag.key, [IsOptional(), IsArray()])
+  sales_channel_id?: string[]
+
+  @IsArray()
+  @IsOptional()
+  category_id?: string[]
+
+  @IsBoolean()
+  @IsOptional()
+  @Transform(({ value }) => optionalBooleanMapper.get(value.toLowerCase()))
+  include_category_children?: boolean
+
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => DateComparisonOperator)
+  created_at?: DateComparisonOperator
+
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => DateComparisonOperator)
+  updated_at?: DateComparisonOperator
+}
---
+++ packages/types/src/modules-sdk/index.ts
@@ -0,0 +1,213 @@
+import { JoinerRelationship, JoinerServiceConfig } from "../joiner"
+
+import { MedusaContainer } from "../common"
+import { RepositoryService } from "../dal"
+import { Logger } from "../logger"
+
+export type Constructor<T> = new (...args: any[]) => T
+export * from "../common/medusa-container"
+
+export type LogLevel =
+  | "query"
+  | "schema"
+  | "error"
+  | "warn"
+  | "info"
+  | "log"
+  | "migration"
+export type LoggerOptions = boolean | "all" | LogLevel[]
+
+export enum MODULE_SCOPE {
+  INTERNAL = "internal",
+  EXTERNAL = "external",
+}
+
+export enum MODULE_RESOURCE_TYPE {
+  SHARED = "shared",
+  ISOLATED = "isolated",
+}
+
+export type InternalModuleDeclaration = {
+  scope: MODULE_SCOPE.INTERNAL
+  resources: MODULE_RESOURCE_TYPE
+  dependencies?: string[]
+  /**
+   * @deprecated The property should not be used.
+   */
+  resolve?: string
+  options?: Record<string, unknown>
+  alias?: string // If multiple modules are registered with the same key, the alias can be used to differentiate them
+  main?: boolean // If the module is the main module for the key when multiple ones are registered
+}
+
+export type ExternalModuleDeclaration = {
+  scope: MODULE_SCOPE.EXTERNAL
+  server?: {
+    type: "http"
+    url: string
+    keepAlive: boolean
+  }
+  options?: Record<string, unknown>
+  alias?: string // If multiple modules are registered with the same key, the alias can be used to differentiate them
+  main?: boolean // If the module is the main module for the key when multiple ones are registered
+}
+
+export type ModuleResolution = {
+  resolutionPath: string | false
+  definition: ModuleDefinition
+  options?: Record<string, unknown>
+  dependencies?: string[]
+  moduleDeclaration?: InternalModuleDeclaration | ExternalModuleDeclaration
+  moduleExports?: ModuleExports
+}
+
+export type ModuleDefinition = {
+  key: string
+  registrationName: string
+  defaultPackage: string | false
+  label: string
+  /**
+   * @deprecated property will be removed in future versions
+   */
+  canOverride?: boolean
+  /**
+   * @deprecated property will be removed in future versions
+   */
+  isRequired?: boolean
+  isQueryable?: boolean // If the module is queryable via Remote Joiner
+  dependencies?: string[]
+  defaultModuleDeclaration:
+    | InternalModuleDeclaration
+    | ExternalModuleDeclaration
+}
+
+export type LinkModuleDefinition = {
+  key: string
+  registrationName: string
+  label: string
+  dependencies?: string[]
+  defaultModuleDeclaration: InternalModuleDeclaration
+}
+
+type ModuleDeclaration = ExternalModuleDeclaration | InternalModuleDeclaration
+export type ModuleConfig = ModuleDeclaration & {
+  module: string
+  path: string
+  definition: ModuleDefinition
+}
+
+export type LoadedModule = unknown & {
+  __joinerConfig: ModuleJoinerConfig
+  __definition: ModuleDefinition
+}
+
+export type LoaderOptions<TOptions = Record<string, unknown>> = {
+  container: MedusaContainer
+  options?: TOptions
+  logger?: Logger
+}
+
+export type ModuleLoaderFunction = (
+  options: LoaderOptions,
+  moduleDeclaration?: InternalModuleDeclaration
+) => Promise<void>
+
+export type ModulesResponse = {
+  module: string
+  resolution: string | false
+}[]
+
+export type ModuleJoinerConfig = Omit<
+  JoinerServiceConfig,
+  "serviceName" | "primaryKeys" | "relationships" | "extends"
+> & {
+  relationships?: ModuleJoinerRelationship[]
+  extends?: {
+    serviceName: string
+    relationship: ModuleJoinerRelationship
+  }[]
+  serviceName?: string
+  primaryKeys?: string[]
+  isLink?: boolean // If the module is a link module
+  linkableKeys?: string[] // Keys that can be used to link to other modules
+  isReadOnlyLink?: boolean // If true it expands a RemoteQuery property but doesn't create a pivot table
+  databaseConfig?: {
+    tableName?: string // Name of the pivot table. If not provided it is auto generated
+    idPrefix?: string // Prefix for the id column. If not provided it is "link"
+    extraFields?: Record<
+      string,
+      {
+        type:
+          | "date"
+          | "time"
+          | "datetime"
+          | "bigint"
+          | "blob"
+          | "uint8array"
+          | "array"
+          | "enumArray"
+          | "enum"
+          | "json"
+          | "integer"
+          | "smallint"
+          | "tinyint"
+          | "mediumint"
+          | "float"
+          | "double"
+          | "boolean"
+          | "decimal"
+          | "string"
+          | "uuid"
+          | "text"
+        defaultValue?: string
+        nullable?: boolean
+        options?: Record<string, unknown> // Mikro-orm options for the column
+      }
+    >
+  }
+}
+
+export declare type ModuleJoinerRelationship = JoinerRelationship & {
+  isInternalService?: boolean // If true, the relationship is an internal service from the medusa core TODO: Remove when there are no more "internal" services
+  deleteCascade?: boolean // If true, the link joiner will cascade deleting the relationship
+}
+
+export type ModuleExports = {
+  service: Constructor<any>
+  loaders?: ModuleLoaderFunction[]
+  migrations?: any[]
+  models?: Constructor<any>[]
+  runMigrations?(
+    options: LoaderOptions,
+    moduleDeclaration?: InternalModuleDeclaration
+  ): Promise<void>
+  revertMigration?(
+    options: LoaderOptions,
+    moduleDeclaration?: InternalModuleDeclaration
+  ): Promise<void>
+}
+
+export interface ModuleServiceInitializeOptions {
+  database: {
+    /**
+     * Forces to use a shared knex connection
+     */
+    connection?: any
+    clientUrl?: string
+    schema?: string
+    host?: string
+    port?: number
+    user?: string
+    password?: string
+    database?: string
+    driverOptions?: Record<string, unknown>
+    debug?: boolean
+  }
+}
+
+export type ModuleServiceInitializeCustomDataLayerOptions = {
+  manager?: any
+  repositories?: {
+    [key: string]: Constructor<RepositoryService>
+  }
+}
