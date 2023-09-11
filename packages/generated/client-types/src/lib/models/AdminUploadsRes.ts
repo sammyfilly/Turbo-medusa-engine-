1c0902f5749983299b11739b7e1424ae4d4a1aec
Sammy filly <136061549+sammyfilly@users.noreply.github.com>
2023-09-11 22:55:58 +0100

    Initialize feat/field-alias-remote-joiner

---
+++ packages/generated/client-types/src/lib/models/AdminUploadsRes.ts
@@ -0,0 +1,20 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+import { SetRelation, Merge } from "../core/ModelUtils"
+
+export interface AdminUploadsRes {
+  /**
+   * Uploaded files details.
+   */
+  uploads: Array<{
+    /**
+     * The URL of the uploaded file.
+     */
+    url: string
+    /**
+     * The key of the file that is identifiable by the file service. It can be used later to retrieve or manipulate the file.
+     */
+    key: string
+  }>
+}
