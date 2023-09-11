24c74a35dc8a863f4717e1a635fd6bf6f8f3d9c6
Sammy filly <136061549+sammyfilly@users.noreply.github.com>
2023-09-11 22:12:20 +0100

    Initialize fix/disable-manage-inventory

---
+++ packages/medusa/src/types/global.ts
@@ -0,0 +1,91 @@
+import {
+  ConfigurableModuleDeclaration,
+  ModuleResolution,
+} from "@medusajs/modules-sdk"
+import { AwilixContainer } from "awilix"
+import { Request } from "express"
+import { LoggerOptions } from "typeorm"
+import { Logger as _Logger } from "winston"
+import { Customer, User } from "../models"
+import { FindConfig, RequestQueryFields } from "./common"
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Express {
+    interface Request {
+      user?: (User | Customer) & { customer_id?: string; userId?: string }
+      scope: MedusaContainer
+      validatedQuery: RequestQueryFields & Record<string, unknown>
+      validatedBody: unknown
+      listConfig: FindConfig<unknown>
+      retrieveConfig: FindConfig<unknown>
+      filterableFields: Record<string, unknown>
+      allowedProperties: string[]
+      includes?: Record<string, boolean>
+      errors: string[]
+    }
+  }
+}
+
+export type ExtendedRequest<TEntity> = Request & { resource: TEntity }
+
+export type ClassConstructor<T> = {
+  new (...args: unknown[]): T
+}
+
+export type MedusaContainer = AwilixContainer & {
+  registerAdd: <T>(name: string, registration: T) => MedusaContainer
+}
+
+export type Logger = _Logger & {
+  progress: (activityId: string, msg: string) => void
+  info: (msg: string) => void
+  warn: (msg: string) => void
+}
+
+export type Constructor<T> = new (...args: any[]) => T
+
+type SessionOptions = {
+  name?: string
+  resave?: boolean
+  rolling?: boolean
+  saveUninitialized?: boolean
+  secret?: string
+  ttl?: number
+}
+
+export type ConfigModule = {
+  projectConfig: {
+    redis_url?: string
+
+    session_options?: SessionOptions
+
+    jwt_secret?: string
+    cookie_secret?: string
+
+    database_url?: string
+    database_type: string
+    database_database?: string
+    database_schema?: string
+    database_logging: LoggerOptions
+
+    database_extra?: Record<string, unknown> & {
+      ssl: { rejectUnauthorized: false }
+    }
+    store_cors?: string
+    admin_cors?: string
+  }
+  featureFlags: Record<string, boolean | string>
+  modules?: Record<
+    string,
+    false | string | Partial<ConfigurableModuleDeclaration>
+  >
+  moduleResolutions?: Record<string, ModuleResolution>
+  plugins: (
+    | {
+        resolve: string
+        options: Record<string, unknown>
+      }
+    | string
+  )[]
+}
