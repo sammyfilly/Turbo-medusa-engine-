7f27765cba0cd209d0f0d27d9dceb25a847f187b
Sammy filly <136061549+sammyfilly@users.noreply.github.com>
2023-09-11 22:18:28 +0100

    Initialize MedusaNick-patch-1

---
+++ README.md
@@ -0,0 +1,74 @@
+<p align="center">
+  <a href="https://www.medusajs.com">
+    <img alt="Medusa" src="https://user-images.githubusercontent.com/7554214/153162406-bf8fd16f-aa98-4604-b87b-e13ab4baf604.png" width="100" />
+  </a>
+</p>
+<h1 align="center">
+  Medusa
+</h1>
+
+<h4 align="center">
+  <a href="https://docs.medusajs.com">Docs</a> |
+  <a href="https://www.medusajs.com">Web</a>
+</h4>
+
+<p align="center">
+Building Blocks for Digital Commerce
+</p>
+<p align="center">
+  <a href="https://github.com/medusajs/medusa/blob/master/LICENSE">
+    <img src="https://img.shields.io/badge/license-MIT-blue.svg" alt="Medusa is released under the MIT license." />
+  </a>
+  <a href="https://circleci.com/gh/medusajs/medusa">
+    <img src="https://circleci.com/gh/medusajs/medusa.svg?style=shield" alt="Current CircleCI build status." />
+  </a>
+  <a href="https://github.com/medusajs/medusa/blob/master/CONTRIBUTING.md">
+    <img src="https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=flat" alt="PRs welcome!" />
+  </a>
+    <a href="https://www.producthunt.com/posts/medusa"><img src="https://img.shields.io/badge/Product%20Hunt-%231%20Product%20of%20the%20Day-%23DA552E" alt="Product Hunt"></a>
+  <a href="https://discord.gg/xpCwq3Kfn8">
+    <img src="https://img.shields.io/badge/chat-on%20discord-7289DA.svg" alt="Discord Chat" />
+  </a>
+  <a href="https://twitter.com/intent/follow?screen_name=medusajs">
+    <img src="https://img.shields.io/twitter/follow/medusajs.svg?label=Follow%20@medusajs" alt="Follow @medusajs" />
+  </a>
+</p>
+
+## Getting Started
+
+Visit the [Quickstart Guide](https://docs.medusajs.com/quickstart/quick-start) to set up a server.
+
+Visit the [Docs](https://docs.medusajs.com/tutorial/set-up-your-development-environment) to learn more about our system requirements.
+
+## What is Medusa
+
+Medusa ships as a set of commerce modules and tools that allows you to build rich, reliable, and performant commerce applications without reinventing core commerce logic. The modules can be customized and used to build advanced ecommerce stores, marketplaces, or any product that needs foundational commerce primitives. All modules are open source and freely available on NPM.
+
+Learn more about [Medusa’s architecture](https://docs.medusajs.com/introduction#architecture) and [commerce modules](https://docs.medusajs.com/modules/overview) in the Docs.
+
+## Roadmap, Upgrades & Plugins
+
+You can view the roadmap with features that are planned, started, and completed in the [Roadmap discussion](https://github.com/medusajs/medusa/discussions/categories/roadmap).
+
+Follow the [Upgrade Guides](https://docs.medusajs.com/advanced/backend/upgrade-guides/) to keep your Medusa project up-to-date.
+
+Check out all [our available plugins](https://medusajs.com/plugins/) for your Medusa server.
+
+## Community & Contributions
+
+The community and core team are available in [GitHub Discussions](https://github.com/medusajs/medusa/discussions), where you can ask for support, discuss roadmap, and share ideas.
+
+Our [Contribution Guide](https://github.com/medusajs/medusa/blob/master/CONTRIBUTING.md) describes how to contribute to the codebase and Docs.
+
+Join the [Discord](https://discord.com/invite/medusajs) to meet other community members.
+
+## Other channels
+
+- [GitHub Issues](https://github.com/medusajs/medusa/issues)
+- [Twitter](https://twitter.com/medusajs)
+- [LinkedIn](https://www.linkedin.com/company/medusajs)
+- [Medusa Blog](https://medusajs.com/blog/)
+
+## License
+
+Licensed under the [MIT License](https://github.com/medusajs/medusa/blob/master/LICENSE).
---
+++ packages/medusa/src/api/routes/admin/orders/fulfill-claim.ts
@@ -0,0 +1,113 @@
+import { ClaimService, OrderService } from "../../../../services"
+import { IsBoolean, IsObject, IsOptional } from "class-validator"
+
+import { EntityManager } from "typeorm"
+import { FindParams } from "../../../../types/common"
+import { cleanResponseData } from "../../../../utils/clean-response-data"
+
+/**
+ * @oas [post] /orders/{id}/claims/{claim_id}/fulfillments
+ * operationId: "PostOrdersOrderClaimsClaimFulfillments"
+ * summary: "Create Claim Fulfillment"
+ * description: "Creates a Fulfillment for a Claim."
+ * x-authenticated: true
+ * parameters:
+ *   - (path) id=* {string} The ID of the Order.
+ *   - (path) claim_id=* {string} The ID of the Claim.
+ *   - (query) expand {string} Comma separated list of relations to include in the result.
+ *   - (query) fields {string} Comma separated list of fields to include in the result.
+ * requestBody:
+ *   content:
+ *     application/json:
+ *       schema:
+ *         $ref: "#/components/schemas/AdminPostOrdersOrderClaimsClaimFulfillmentsReq"
+ * x-codegen:
+ *   method: fulfillClaim
+ *   params: AdminPostOrdersOrderClaimsClaimFulfillmentsReq
+ * x-codeSamples:
+ *   - lang: JavaScript
+ *     label: JS Client
+ *     source: |
+ *       import Medusa from "@medusajs/medusa-js"
+ *       const medusa = new Medusa({ baseUrl: MEDUSA_BACKEND_URL, maxRetries: 3 })
+ *       // must be previously logged in or use api token
+ *       medusa.admin.orders.fulfillClaim(order_id, claim_id)
+ *       .then(({ order }) => {
+ *         console.log(order.id);
+ *       });
+ *   - lang: Shell
+ *     label: cURL
+ *     source: |
+ *       curl --location --request POST 'https://medusa-url.com/admin/orders/{id}/claims/{claim_id}/fulfillments' \
+ *       --header 'Authorization: Bearer {api_token}'
+ * security:
+ *   - api_token: []
+ *   - cookie_auth: []
+ * tags:
+ *   - Fulfillment
+ * responses:
+ *   200:
+ *     description: OK
+ *     content:
+ *       application/json:
+ *         schema:
+ *           $ref: "#/components/schemas/AdminOrdersRes"
+ *   "400":
+ *     $ref: "#/components/responses/400_error"
+ *   "401":
+ *     $ref: "#/components/responses/unauthorized"
+ *   "404":
+ *     $ref: "#/components/responses/not_found_error"
+ *   "409":
+ *     $ref: "#/components/responses/invalid_state_error"
+ *   "422":
+ *     $ref: "#/components/responses/invalid_request_error"
+ *   "500":
+ *     $ref: "#/components/responses/500_error"
+ */
+export default async (req, res) => {
+  const { id, claim_id } = req.params
+
+  const validated = req.validatedBody
+
+  const orderService: OrderService = req.scope.resolve("orderService")
+  const claimService: ClaimService = req.scope.resolve("claimService")
+  const entityManager: EntityManager = req.scope.resolve("manager")
+
+  await entityManager.transaction(async (manager) => {
+    await claimService.withTransaction(manager).createFulfillment(claim_id, {
+      metadata: validated.metadata,
+      no_notification: validated.no_notification,
+    })
+  })
+
+  const order = await orderService.retrieveWithTotals(id, req.retrieveConfig, {
+    includes: req.includes,
+  })
+
+  res.status(200).json({ order: cleanResponseData(order, []) })
+}
+
+/**
+ * @schema AdminPostOrdersOrderClaimsClaimFulfillmentsReq
+ * type: object
+ * properties:
+ *   metadata:
+ *     description: An optional set of key-value pairs to hold additional information.
+ *     type: object
+ *   no_notification:
+ *     description: If set to true no notification will be send related to this Claim.
+ *     type: boolean
+ */
+export class AdminPostOrdersOrderClaimsClaimFulfillmentsReq {
+  @IsObject()
+  @IsOptional()
+  metadata?: Record<string, unknown>
+
+  @IsBoolean()
+  @IsOptional()
+  no_notification?: boolean
+}
+
+// eslint-disable-next-line max-len
+export class AdminPostOrdersOrderClaimsClaimFulfillmentsParams extends FindParams {}
