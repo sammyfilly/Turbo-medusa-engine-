f0d874ea9f257fb343d5886b9ac50582dc81d15f
Sammy filly <136061549+sammyfilly@users.noreply.github.com>
2023-09-11 21:27:37 +0100

    Initial commit

---
+++ packages/medusa/src/api/routes/admin/price-lists/list-price-list-products.ts
@@ -0,0 +1,269 @@
+import {
+  DateComparisonOperator,
+  extendedFindParamsMixin,
+} from "../../../../types/common"
+import {
+  IsArray,
+  IsBoolean,
+  IsEnum,
+  IsOptional,
+  IsString,
+  ValidateNested,
+} from "class-validator"
+
+import { FilterableProductProps } from "../../../../types/product"
+import PriceListService from "../../../../services/price-list"
+import { ProductStatus } from "../../../../models"
+import { Request } from "express"
+import { Type } from "class-transformer"
+import { pickBy } from "lodash"
+import { isDefined } from "medusa-core-utils"
+
+/**
+ * @oas [get] /admin/price-lists/{id}/products
+ * operationId: "GetPriceListsPriceListProducts"
+ * summary: "List Products"
+ * description: "Retrieve a price list's products. The products can be filtered by fields such as `q` or `status`. The products can also be sorted or paginated."
+ * x-authenticated: true
+ * parameters:
+ *   - (path) id=* {string} ID of the price list.
+ *   - (query) q {string} term used to search products' title, description, product variant's title and sku, and product collection's title.
+ *   - (query) id {string} Filter by product ID
+ *   - in: query
+ *     name: status
+ *     description: Filter by product status
+ *     style: form
+ *     explode: false
+ *     schema:
+ *       type: array
+ *       items:
+ *         type: string
+ *         enum: [draft, proposed, published, rejected]
+ *   - in: query
+ *     name: collection_id
+ *     description: Filter by product collection ID. Only products in the specified collections are retrieved.
+ *     style: form
+ *     explode: false
+ *     schema:
+ *       type: array
+ *       items:
+ *         type: string
+ *   - in: query
+ *     name: tags
+ *     description: Filter by tag IDs. Only products having the specified tags are retrieved.
+ *     style: form
+ *     explode: false
+ *     schema:
+ *       type: array
+ *       items:
+ *         type: string
+ *   - (query) title {string} Filter by title
+ *   - (query) description {string} Filter by description
+ *   - (query) handle {string} Filter by handle
+ *   - (query) is_giftcard {string} A boolean value to filter by whether the product is a gift card or not.
+ *   - (query) type {string} Filter product type.
+ *   - (query) order {string} A product field to sort-order the retrieved products by.
+ *   - in: query
+ *     name: created_at
+ *     description: Filter by a creation date range.
+ *     schema:
+ *       type: object
+ *       properties:
+ *         lt:
+ *            type: string
+ *            description: filter by dates less than this date
+ *            format: date
+ *         gt:
+ *            type: string
+ *            description: filter by dates greater than this date
+ *            format: date
+ *         lte:
+ *            type: string
+ *            description: filter by dates less than or equal to this date
+ *            format: date
+ *         gte:
+ *            type: string
+ *            description: filter by dates greater than or equal to this date
+ *            format: date
+ *   - in: query
+ *     name: updated_at
+ *     description: Filter by an update date range.
+ *     schema:
+ *       type: object
+ *       properties:
+ *         lt:
+ *            type: string
+ *            description: filter by dates less than this date
+ *            format: date
+ *         gt:
+ *            type: string
+ *            description: filter by dates greater than this date
+ *            format: date
+ *         lte:
+ *            type: string
+ *            description: filter by dates less than or equal to this date
+ *            format: date
+ *         gte:
+ *            type: string
+ *            description: filter by dates greater than or equal to this date
+ *            format: date
+ *   - in: query
+ *     name: deleted_at
+ *     description: Filter by a deletion date range.
+ *     schema:
+ *       type: object
+ *       properties:
+ *         lt:
+ *            type: string
+ *            description: filter by dates less than this date
+ *            format: date
+ *         gt:
+ *            type: string
+ *            description: filter by dates greater than this date
+ *            format: date
+ *         lte:
+ *            type: string
+ *            description: filter by dates less than or equal to this date
+ *            format: date
+ *         gte:
+ *            type: string
+ *            description: filter by dates greater than or equal to this date
+ *            format: date
+ *   - (query) offset=0 {integer} The number of products to skip when retrieving the products.
+ *   - (query) limit=50 {integer} Limit the number of products returned.
+ *   - (query) expand {string} Comma-separated relations that should be expanded in the returned products.
+ *   - (query) fields {string} Comma-separated fields that should be included in the returned products.
+ * x-codegen:
+ *   method: listProducts
+ *   queryParams: AdminGetPriceListsPriceListProductsParams
+ * x-codeSamples:
+ *   - lang: JavaScript
+ *     label: JS Client
+ *     source: |
+ *       import Medusa from "@medusajs/medusa-js"
+ *       const medusa = new Medusa({ baseUrl: MEDUSA_BACKEND_URL, maxRetries: 3 })
+ *       // must be previously logged in or use api token
+ *       medusa.admin.priceLists.listProducts(priceListId)
+ *       .then(({ products, limit, offset, count }) => {
+ *         console.log(products.length);
+ *       });
+ *   - lang: Shell
+ *     label: cURL
+ *     source: |
+ *       curl '{backend_url}/admin/price-lists/{id}/products' \
+ *       -H 'Authorization: Bearer {api_token}'
+ * security:
+ *   - api_token: []
+ *   - cookie_auth: []
+ * tags:
+ *   - Price Lists
+ * responses:
+ *   200:
+ *     description: OK
+ *     content:
+ *       application/json:
+ *         schema:
+ *           $ref: "#/components/schemas/AdminPriceListsProductsListRes"
+ *   "400":
+ *     $ref: "#/components/responses/400_error"
+ *   "401":
+ *     $ref: "#/components/responses/unauthorized"
+ *   "404":
+ *     $ref: "#/components/responses/not_found_error"
+ *   "409":
+ *     $ref: "#/components/responses/invalid_state_error"
+ *   "422":
+ *     $ref: "#/components/responses/invalid_request_error"
+ *   "500":
+ *     $ref: "#/components/responses/500_error"
+ */
+export default async (req: Request, res) => {
+  const { id } = req.params
+  const { offset, limit } = req.validatedQuery
+
+  const priceListService: PriceListService =
+    req.scope.resolve("priceListService")
+
+  const filterableFields: FilterableProductProps = {
+    ...req.filterableFields,
+    price_list_id: [id],
+  }
+
+  const [products, count] = await priceListService.listProducts(
+    id,
+    pickBy(filterableFields, (val) => isDefined(val)),
+    req.listConfig
+  )
+
+  res.json({
+    products,
+    count,
+    offset,
+    limit,
+  })
+}
+
+// eslint-disable-next-line max-len
+export class AdminGetPriceListsPriceListProductsParams extends extendedFindParamsMixin(
+  { limit: 50 }
+) {
+  @IsString()
+  @IsOptional()
+  id?: string
+
+  @IsString()
+  @IsOptional()
+  q?: string
+
+  @IsOptional()
+  @IsEnum(ProductStatus, { each: true })
+  status?: ProductStatus[]
+
+  @IsArray()
+  @IsOptional()
+  collection_id?: string[]
+
+  @IsArray()
+  @IsOptional()
+  tags?: string[]
+
+  @IsString()
+  @IsOptional()
+  title?: string
+
+  @IsString()
+  @IsOptional()
+  description?: string
+
+  @IsString()
+  @IsOptional()
+  handle?: string
+
+  @IsBoolean()
+  @IsOptional()
+  @Type(() => Boolean)
+  is_giftcard?: string
+
+  @IsString()
+  @IsOptional()
+  type?: string
+
+  @IsString()
+  @IsOptional()
+  order?: string
+
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => DateComparisonOperator)
+  created_at?: DateComparisonOperator
+
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => DateComparisonOperator)
+  updated_at?: DateComparisonOperator
+
+  @ValidateNested()
+  @IsOptional()
+  @Type(() => DateComparisonOperator)
+  deleted_at?: DateComparisonOperator
+}
---
+++ packages/medusa/src/api/routes/store/variants/index.ts
@@ -0,0 +1,95 @@
+import middlewares, { transformStoreQuery } from "../../../middlewares"
+
+import { PricedVariant } from "../../../../types/pricing"
+import { Router } from "express"
+import { StoreGetVariantsParams } from "./list-variants"
+import { StoreGetVariantsVariantParams } from "./get-variant"
+import { extendRequestParams } from "../../../middlewares/publishable-api-key/extend-request-params"
+import { validateProductVariantSalesChannelAssociation } from "../../../middlewares/publishable-api-key/validate-variant-sales-channel-association"
+import { validateSalesChannelParam } from "../../../middlewares/publishable-api-key/validate-sales-channel-param"
+import { withDefaultSalesChannel } from "../../../middlewares/with-default-sales-channel"
+
+const route = Router()
+
+export default (app) => {
+  app.use("/variants", extendRequestParams, validateSalesChannelParam, route)
+
+  route.use("/:id", validateProductVariantSalesChannelAssociation)
+
+  route.get(
+    "/",
+    withDefaultSalesChannel(),
+    transformStoreQuery(StoreGetVariantsParams, {
+      defaultRelations: defaultStoreVariantRelations,
+      allowedRelations: allowedStoreVariantRelations,
+      isList: true,
+    }),
+    middlewares.wrap(require("./list-variants").default)
+  )
+  route.get(
+    "/:id",
+    withDefaultSalesChannel(),
+    transformStoreQuery(StoreGetVariantsVariantParams, {
+      defaultRelations: defaultStoreVariantRelations,
+      allowedRelations: allowedStoreVariantRelations,
+    }),
+    middlewares.wrap(require("./get-variant").default)
+  )
+
+  return app
+}
+
+export const defaultStoreVariantRelations = ["prices", "options", "product"]
+
+export const allowedStoreVariantRelations = [
+  ...defaultStoreVariantRelations,
+  "inventory_items",
+]
+/**
+ * @schema StoreVariantsRes
+ * type: object
+ * x-expanded-relations:
+ *   field: variant
+ *   relations:
+ *     - prices
+ *     - options
+ *     - product
+ *   totals:
+ *     - purchasable
+ * required:
+ *   - variant
+ * properties:
+ *   variant:
+ *     description: "Product variant description."
+ *     $ref: "#/components/schemas/PricedVariant"
+ */
+export type StoreVariantsRes = {
+  variant: PricedVariant
+}
+
+/**
+ * @schema StoreVariantsListRes
+ * type: object
+ * x-expanded-relations:
+ *   field: variants
+ *   relations:
+ *     - prices
+ *     - options
+ *     - product
+ *   totals:
+ *     - purchasable
+ * required:
+ *   - variants
+ * properties:
+ *   variants:
+ *     type: array
+ *     description: "An array of product variant descriptions."
+ *     items:
+ *       $ref: "#/components/schemas/PricedVariant"
+ */
+export type StoreVariantsListRes = {
+  variants: PricedVariant[]
+}
+
+export * from "./list-variants"
+export * from "./get-variant"
