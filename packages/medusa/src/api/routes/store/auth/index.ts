4684a42187ad272b164032088bac56cefd11534d
Sammy filly <136061549+sammyfilly@users.noreply.github.com>
2023-09-11 21:54:05 +0100

    Initialize docs/note-yarn

---
+++ packages/medusa/src/api/routes/store/auth/index.ts
@@ -0,0 +1,33 @@
+import { Router } from "express"
+import middlewares from "../../../middlewares"
+import { Customer } from "./../../../.."
+
+const route = Router()
+
+export default (app) => {
+  app.use("/auth", route)
+
+  route.get(
+    "/",
+    middlewares.authenticate(),
+    middlewares.wrap(require("./get-session").default)
+  )
+  route.get("/:email", middlewares.wrap(require("./exists").default))
+  route.delete("/", middlewares.wrap(require("./delete-session").default))
+  route.post("/", middlewares.wrap(require("./create-session").default))
+
+  return app
+}
+
+export type StoreAuthRes = {
+  customer: Customer
+}
+
+export type StoreGetAuthEmailRes = {
+  exists: boolean
+}
+
+export * from "./create-session"
+export * from "./delete-session"
+export * from "./exists"
+export * from "./get-session"
