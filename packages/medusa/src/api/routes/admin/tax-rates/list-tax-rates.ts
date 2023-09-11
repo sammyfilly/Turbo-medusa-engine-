74e043f59f9dba7ff8f6447d2f7c507ac2499e5f
Sammy filly <136061549+sammyfilly@users.noreply.github.com>
2023-09-11 22:51:15 +0100

    Initialize docs/fix-readme

---
+++ packages/medusa/src/api/routes/admin/tax-rates/list-tax-rates.ts
@@ -0,0 +1,177 @@
+import { IsArray, IsNumber, IsOptional, IsString } from "class-validator"
+import { getListConfig, pickByConfig } from "./utils/get-query-config"
+import { identity, omit, pickBy } from "lodash"
+
+import { IsType } from "../../../../utils/validators/is-type"
+import { NumericalComparisonOperator } from "../../../../types/common"
+import { TaxRate } from "../../../.."
+import { TaxRateService } from "../../../../services"
+import { Type } from "class-transformer"
+import { validator } from "../../../../utils/validator"
+
+/**
+ * @oas [get] /admin/tax-rates
+ * operationId: "GetTaxRates"
+ * summary: "List Tax Rates"
+ * description: "Retrieve a list of Tax Rates. The tax rates can be filtered by fields such as `name` or `rate`. The tax rates can also be paginated."
+ * x-authenticated: true
+ * parameters:
+ *   - (query) name {string} Filter by name.
+ *   - in: query
+ *     name: region_id
+ *     style: form
+ *     explode: false
+ *     description: Filter by Region IDs
+ *     schema:
+ *       oneOf:
+ *        - type: string
+ *        - type: array
+ *          items:
+ *            type: string
+ *   - (query) code {string} Filter by code.
+ *   - in: query
+ *     name: rate
+ *     style: form
+ *     explode: false
+ *     description: Filter by Rate
+ *     schema:
+ *       oneOf:
+ *        - type: number
+ *        - type: object
+ *          properties:
+ *            lt:
+ *              type: number
+ *              description: filter by rates less than this number
+ *            gt:
+ *              type: number
+ *              description: filter by rates greater than this number
+ *            lte:
+ *              type: number
+ *              description: filter by rates less than or equal to this number
+ *            gte:
+ *              type: number
+ *              description: filter by rates greater than or equal to this number
+ *   - (query) offset=0 {integer} The number of tax rates to skip when retrieving the tax rates.
+ *   - (query) limit=50 {integer} Limit the number of tax rates returned.
+ *   - in: query
+ *     name: fields
+ *     description: "Comma-separated fields that should be included in the returned tax rate."
+ *     style: form
+ *     explode: false
+ *     schema:
+ *       type: array
+ *       items:
+ *         type: string
+ *   - in: query
+ *     name: expand
+ *     description: "Comma-separated relations that should be expanded in the returned tax rate."
+ *     style: form
+ *     explode: false
+ *     schema:
+ *       type: array
+ *       items:
+ *         type: string
+ * x-codegen:
+ *   method: list
+ *   queryParams: AdminGetTaxRatesParams
+ * x-codeSamples:
+ *   - lang: JavaScript
+ *     label: JS Client
+ *     source: |
+ *       import Medusa from "@medusajs/medusa-js"
+ *       const medusa = new Medusa({ baseUrl: MEDUSA_BACKEND_URL, maxRetries: 3 })
+ *       // must be previously logged in or use api token
+ *       medusa.admin.taxRates.list()
+ *       .then(({ tax_rates, limit, offset, count }) => {
+ *         console.log(tax_rates.length);
+ *       });
+ *   - lang: Shell
+ *     label: cURL
+ *     source: |
+ *       curl '{backend_url}/admin/tax-rates' \
+ *       -H 'Authorization: Bearer {api_token}'
+ * security:
+ *   - api_token: []
+ *   - cookie_auth: []
+ * tags:
+ *   - Tax Rates
+ * responses:
+ *   200:
+ *     description: OK
+ *     content:
+ *       application/json:
+ *         schema:
+ *           $ref: "#/components/schemas/AdminTaxRatesListRes"
+ *   "400":
+ *     $ref: "#/components/responses/400_error"
+ *   "401":
+ *     $ref: "#/components/responses/unauthorized"
+ *   "404":
+ *     $ref: "#/components/responses/not_found_error"
+ *   "409":
+ *     $ref: "#/components/responses/invalid_state_error"
+ *   "422":
+ *     $ref: "#/components/responses/invalid_request_error"
+ *   "500":
+ *     $ref: "#/components/responses/500_error"
+ */
+export default async (req, res) => {
+  const value = await validator(AdminGetTaxRatesParams, req.query)
+
+  const rateService: TaxRateService = req.scope.resolve("taxRateService")
+
+  const listConfig = getListConfig()
+
+  const filterableFields = omit(value, [
+    "limit",
+    "offset",
+    "expand",
+    "fields",
+    "order",
+  ])
+
+  const [rates, count] = await rateService.listAndCount(
+    pickBy(filterableFields, identity),
+    listConfig
+  )
+
+  const data = pickByConfig<TaxRate>(rates, listConfig)
+
+  res.json({ tax_rates: data, count, offset: value.offset, limit: value.limit })
+}
+
+export class AdminGetTaxRatesParams {
+  @IsOptional()
+  @IsType([String, [String]])
+  region_id?: string | string[]
+
+  @IsString()
+  @IsOptional()
+  name?: string
+
+  @IsString()
+  @IsOptional()
+  code?: string
+
+  @IsType([NumericalComparisonOperator, Number])
+  @IsOptional()
+  rate?: number | NumericalComparisonOperator
+
+  @IsNumber()
+  @IsOptional()
+  @Type(() => Number)
+  offset? = 0
+
+  @IsNumber()
+  @IsOptional()
+  @Type(() => Number)
+  limit? = 50
+
+  @IsArray()
+  @IsOptional()
+  expand?: string[]
+
+  @IsArray()
+  @IsOptional()
+  fields?: string[]
+}
---
+++ www/docs/src/theme/MDXComponents/index.tsx
@@ -0,0 +1,15 @@
+// Import the original mapper
+import MDXComponents from "@theme-original/MDXComponents"
+import MDXInlineCode from "@site/src/components/MDXComponents/InlineCode"
+import CloudinaryImage from "@site/src/components/CloudinaryImage"
+import MDXDetails from "./Details"
+import MDXSummary from "./Summary"
+
+export default {
+  // Re-use the default mapping
+  ...MDXComponents,
+  inlineCode: MDXInlineCode,
+  img: CloudinaryImage,
+  details: MDXDetails,
+  summary: MDXSummary,
+}
