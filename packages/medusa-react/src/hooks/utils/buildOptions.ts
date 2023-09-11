13923b8376aac2c638c25d7ef5379ab28bc83e9b
Sammy filly <136061549+sammyfilly@users.noreply.github.com>
2023-09-11 22:03:03 +0100

    Initialize chore/fix-prodcat-oas

---
+++ packages/medusa-react/src/hooks/utils/buildOptions.ts
@@ -0,0 +1,30 @@
+import {
+  QueryClient,
+  QueryKey,
+  UseMutationOptions,
+} from "@tanstack/react-query"
+
+export const buildOptions = <
+  TData,
+  TError,
+  TVariables,
+  TContext,
+  TKey extends QueryKey
+>(
+  queryClient: QueryClient,
+  queryKey?: TKey,
+  options?: UseMutationOptions<TData, TError, TVariables, TContext>
+): UseMutationOptions<TData, TError, TVariables, TContext> => {
+  return {
+    ...options,
+    onSuccess: (...args) => {
+      if (options?.onSuccess) {
+        return options.onSuccess(...args)
+      }
+
+      if (queryKey !== undefined) {
+        queryClient.invalidateQueries(queryKey)
+      }
+    },
+  }
+}
