9634bc7c313574be8f0c978774d6dff7a4b4f71a
Sammy filly <136061549+sammyfilly@users.noreply.github.com>
2023-09-11 21:53:29 +0100

    Initialize feat/payment_collection_react_hooks

---
+++ integration-tests/api/factories/simple-customer-factory.ts
@@ -0,0 +1,57 @@
+import { Customer } from "@medusajs/medusa"
+import faker from "faker"
+import { Connection } from "typeorm"
+import {
+  CustomerGroupFactoryData,
+  simpleCustomerGroupFactory
+} from "./simple-customer-group-factory"
+
+export type CustomerFactoryData = {
+  id?: string
+  email?: string
+  groups?: CustomerGroupFactoryData[]
+  password_hash?: string
+  has_account?: boolean
+}
+
+export const simpleCustomerFactory = async (
+  connection: Connection,
+  data: CustomerFactoryData = {},
+  seed?: number
+): Promise<Customer> => {
+  if (typeof seed !== "undefined") {
+    faker.seed(seed)
+  }
+
+  const manager = connection.manager
+
+  const customerId = data.id || `simple-customer-${Math.random() * 1000}`
+  const c = manager.create(Customer, {
+    id: customerId,
+    email: data.email,
+    password_hash:
+      data.password_hash ??
+      "c2NyeXB0AAEAAAABAAAAAVMdaddoGjwU1TafDLLlBKnOTQga7P2dbrfgf3fB+rCD/cJOMuGzAvRdKutbYkVpuJWTU39P7OpuWNkUVoEETOVLMJafbI8qs8Qx/7jMQXkN", // password matching "test"
+    has_account: data.has_account ?? true,
+  })
+
+  if (data.password_hash) {
+    c.password_hash = data.password_hash
+    c.has_account = true
+  }
+
+  const customer = await manager.save(c)
+
+  if (data.groups) {
+    const groups = []
+    for (const g of data.groups) {
+      const created = await simpleCustomerGroupFactory(connection, g)
+      groups.push(created)
+    }
+
+    customer.groups = groups
+    await manager.save(customer)
+  }
+
+  return customer
+}
