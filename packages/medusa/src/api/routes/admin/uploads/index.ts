1e67b82ed0383be765551435975886563333d6c9
Sammy filly <136061549+sammyfilly@users.noreply.github.com>
2023-09-11 21:30:33 +0100

    Initialize feat/upload-to-storage

---
+++ packages/medusa/src/api/routes/admin/uploads/index.ts
@@ -0,0 +1,45 @@
+import { Router } from "express"
+import multer from "multer"
+import { DeleteResponse } from "../../../../types/common"
+
+import middlewares from "../../../middlewares"
+
+const route = Router()
+const upload = multer({ dest: "uploads/" })
+
+export default (app) => {
+  app.use("/uploads", route)
+
+  route.post(
+    "/",
+    upload.array("files"),
+    middlewares.wrap(require("./create-upload").default)
+  )
+
+  route.post(
+    "/stream",
+    middlewares.wrap(require("./generate-and-upload-csv").default)
+  )
+
+  route.post(
+    "/stream-file",
+    upload.single("file"),
+    middlewares.wrap(require("./upload-csv-file").default)
+  )
+
+  route.post("/download", middlewares.wrap(require("./download-csv").default))
+
+  // removed on purpose
+  // route.post("/delete", middlewares.wrap(require("./delete-upload").default))
+
+  return app
+}
+
+export type AdminUploadRes = {
+  uploads: unknown[]
+}
+
+export type AdminDeleteUploadRes = DeleteResponse
+
+export * from "./create-upload"
+// export * from "./delete-upload"
