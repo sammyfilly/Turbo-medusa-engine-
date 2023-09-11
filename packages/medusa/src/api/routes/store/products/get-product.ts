d72da37d66ea91163c0f2510fb171dc8f2931ec0
Sammy filly <136061549+sammyfilly@users.noreply.github.com>
2023-09-11 22:54:41 +0100

    Initialize feat/get-product-store-isolated-product-module

---
+++ packages/medusa/src/api/routes/store/products/get-product.ts
@@ -0,0 +1,261 @@
+import {
+  CartService,
+  PricingService,
+  ProductService,
+  ProductVariantInventoryService,
+  RegionService,
+  ShippingProfileService,
+} from "../../../../services"
+import { IsOptional, IsString } from "class-validator"
+
+import { PriceSelectionParams } from "../../../../types/price-selection"
+import { cleanResponseData } from "../../../../utils/clean-response-data"
+import IsolateProductDomain from "../../../../loaders/feature-flags/isolate-product-domain"
+import { defaultStoreProductsFields } from "./index"
+
+/**
+ * @oas [get] /store/products/{id}
+ * operationId: GetProductsProduct
+ * summary: Get a Product
+ * description: |
+ *   Retrieve a Product's details. For accurate and correct pricing of the product based on the customer's context, it's highly recommended to pass fields such as
+ *   `region_id`, `currency_code`, and `cart_id` when available.
+ *
+ *   Passing `sales_channel_id` ensures retrieving only products available in the current sales channel.
+ *   You can alternatively use a publishable API key in the request header instead of passing a `sales_channel_id`.
+ * externalDocs:
+ *   description: "How to pass product pricing parameters"
+ *   url: "https://docs.medusajs.com/modules/products/storefront/show-products#product-pricing-parameters"
+ * parameters:
+ *   - (path) id=* {string} The ID of the Product.
+ *   - (query) sales_channel_id {string} The ID of the sales channel the customer is viewing the product from.
+ *   - (query) cart_id {string} The ID of the cart. This is useful for accurate pricing based on the cart's context.
+ *   - (query) region_id {string} The ID of the region. This is useful for accurate pricing based on the selected region.
+ *   - (query) expand {string} Comma-separated relations that should be expanded in the returned product.
+ *   - (query) fields {string} Comma-separated fields that should be included in the returned product.
+ *   - in: query
+ *     name: currency_code
+ *     style: form
+ *     explode: false
+ *     description: A 3 character ISO currency code. This is useful for accurate pricing based on the selected currency.
+ *     schema:
+ *       type: string
+ *       externalDocs:
+ *         url: https://en.wikipedia.org/wiki/ISO_4217#Active_codes
+ *         description: See a list of codes.
+ * x-codegen:
+ *   method: retrieve
+ *   queryParams: StoreGetProductsProductParams
+ * x-codeSamples:
+ *   - lang: JavaScript
+ *     label: JS Client
+ *     source: |
+ *       import Medusa from "@medusajs/medusa-js"
+ *       const medusa = new Medusa({ baseUrl: MEDUSA_BACKEND_URL, maxRetries: 3 })
+ *       medusa.products.retrieve(productId)
+ *       .then(({ product }) => {
+ *         console.log(product.id);
+ *       });
+ *   - lang: Shell
+ *     label: cURL
+ *     source: |
+ *       curl '{backend_url}/store/products/{id}'
+ * tags:
+ *   - Products
+ * responses:
+ *   200:
+ *     description: OK
+ *     content:
+ *       application/json:
+ *         schema:
+ *           $ref: "#/components/schemas/StoreProductsRes"
+ *   "400":
+ *     $ref: "#/components/responses/400_error"
+ *   "404":
+ *     $ref: "#/components/responses/not_found_error"
+ *   "409":
+ *     $ref: "#/components/responses/invalid_state_error"
+ *   "422":
+ *     $ref: "#/components/responses/invalid_request_error"
+ *   "500":
+ *     $ref: "#/components/responses/500_error"
+ */
+export default async (req, res) => {
+  const { id } = req.params
+
+  const validated = req.validatedQuery as StoreGetProductsProductParams
+
+  const customer_id = req.user?.customer_id
+
+  const productVariantInventoryService: ProductVariantInventoryService =
+    req.scope.resolve("productVariantInventoryService")
+  const productService: ProductService = req.scope.resolve("productService")
+  const pricingService: PricingService = req.scope.resolve("pricingService")
+  const cartService: CartService = req.scope.resolve("cartService")
+  const regionService: RegionService = req.scope.resolve("regionService")
+  const featureFlagRouter = req.scope.resolve("featureFlagRouter")
+
+  let rawProduct
+  if (featureFlagRouter.isFeatureEnabled(IsolateProductDomain.key)) {
+    rawProduct = await getProductWithIsolatedProductModule(req, id)
+  } else {
+    rawProduct = await productService.retrieve(id, req.retrieveConfig)
+  }
+
+  let sales_channel_id = validated.sales_channel_id
+  if (req.publishableApiKeyScopes?.sales_channel_ids.length === 1) {
+    sales_channel_id = req.publishableApiKeyScopes.sales_channel_ids[0]
+  }
+
+  let regionId = validated.region_id
+  let currencyCode = validated.currency_code
+  if (validated.cart_id) {
+    const cart = await cartService.retrieve(validated.cart_id, {
+      select: ["id", "region_id"],
+    })
+    const region = await regionService.retrieve(cart.region_id, {
+      select: ["id", "currency_code"],
+    })
+    regionId = region.id
+    currencyCode = region.currency_code
+  }
+
+  const decoratedProduct = rawProduct
+
+  // We only set prices if variants.prices are requested
+  const shouldSetPricing = ["variants", "variants.prices"].every((relation) =>
+    req.retrieveConfig.relations?.includes(relation)
+  )
+
+  // We only set availability if variants are requested
+  const shouldSetAvailability =
+    req.retrieveConfig.relations?.includes("variants")
+
+  const decoratePromises: Promise<any>[] = []
+
+  if (shouldSetPricing) {
+    decoratePromises.push(
+      pricingService.setProductPrices([decoratedProduct], {
+        cart_id: validated.cart_id,
+        customer_id: customer_id,
+        region_id: regionId,
+        currency_code: currencyCode,
+        include_discount_prices: true,
+      })
+    )
+  }
+
+  if (shouldSetAvailability) {
+    decoratePromises.push(
+      productVariantInventoryService.setProductAvailability(
+        [decoratedProduct],
+        sales_channel_id
+      )
+    )
+  }
+
+  // We can run them concurrently as the new properties are assigned to the references
+  // of the appropriate entity
+  await Promise.all(decoratePromises)
+
+  res.json({
+    product: cleanResponseData(decoratedProduct, req.allowedProperties || []),
+  })
+}
+
+async function getProductWithIsolatedProductModule(req, id: string) {
+  const remoteQuery = req.scope.resolve("remoteQuery")
+
+  const variables = { id }
+  const commonProperties = ["id", "created_at", "updated_at", "deleted_at"]
+
+  const query = `
+      query ($id: String!) {
+        product (id: $id) {
+          ${defaultStoreProductsFields.join("\n")}
+          images {
+            ${commonProperties.join("\n")}
+            url
+            metadata
+          }
+          tags {
+            ${commonProperties.join("\n")}
+            value
+          }
+          type {
+            ${commonProperties.join("\n")}
+            value
+          }
+          collection {
+            title
+            handle
+            ${commonProperties.join("\n")}
+          }
+          options {
+            ${commonProperties.join("\n")}
+            title
+            product_id
+            metadata
+            values {
+              ${commonProperties.join("\n")}
+              value
+              option_id
+              variant_id
+              metadata
+            }
+          }
+          variants {
+            ${commonProperties.join("\n")}
+            title
+            product_id
+            sku
+            barcode
+            ean
+            upc
+            variant_rank
+            inventory_quantity
+            allow_backorder
+            manage_inventory
+            hs_code
+            origin_country
+            mid_code
+            material
+            weight
+            length
+            height
+            width
+            metadata
+            options {
+              ${commonProperties.join("\n")}
+              value
+              option_id
+              variant_id
+              metadata
+            }
+          }
+        } 
+      }
+    `
+
+  const [product] = await remoteQuery(query, variables)
+
+  // TODO: Waiting for pr #4990 before being able to remove that logic and replace it by a new relation in the query above
+  const shippingProfileService = req.scope.resolve(
+    "shippingProfileService"
+  ) as ShippingProfileService
+
+  const profilesByProductId = await shippingProfileService.retrieveForProducts([
+    product.id,
+  ])
+
+  product.profile_id = profilesByProductId[product.id]?.[0]?.id
+  product.profiles = profilesByProductId[product.id]
+
+  return product
+}
+
+export class StoreGetProductsProductParams extends PriceSelectionParams {
+  @IsString()
+  @IsOptional()
+  sales_channel_id?: string
+}
---
+++ packages/orchestration/CHANGELOG.md
@@ -0,0 +1,55 @@
+# @medusajs/orchestration
+
+## 0.3.0
+
+### Minor Changes
+
+- [#4695](https://github.com/medusajs/medusa/pull/4695) [`4d16acf5f`](https://github.com/medusajs/medusa/commit/4d16acf5f096b5656b645f510f9c971e7c2dc9ef) Thanks [@carlos-r-l-rodrigues](https://github.com/carlos-r-l-rodrigues)! - Medusa App Loader
+
+- [#4695](https://github.com/medusajs/medusa/pull/4695) [`4d16acf5f`](https://github.com/medusajs/medusa/commit/4d16acf5f096b5656b645f510f9c971e7c2dc9ef) Thanks [@carlos-r-l-rodrigues](https://github.com/carlos-r-l-rodrigues)! - introduce @medusajs/link-modules
+
+- [#4925](https://github.com/medusajs/medusa/pull/4925) [`a4906d0ac`](https://github.com/medusajs/medusa/commit/a4906d0ac0af36b1382d3befe64281b404387bd7) Thanks [@carlos-r-l-rodrigues](https://github.com/carlos-r-l-rodrigues)! - Use MedusaApp on core and initial JoinerConfig for internal services
+
+### Patch Changes
+
+- [#4930](https://github.com/medusajs/medusa/pull/4930) [`c3dba0694`](https://github.com/medusajs/medusa/commit/c3dba069488952945150117a30b1306a2e0bb3ce) Thanks [@riqwan](https://github.com/riqwan)! - fix(modules-sdk, orchestration): add missing dependencies
+
+- Updated dependencies [[`460161a69`](https://github.com/medusajs/medusa/commit/460161a69f22cf6d561952e92e7d9b56912113e6), [`fcb6b4f51`](https://github.com/medusajs/medusa/commit/fcb6b4f510dba2757570625acb5da9476b7544fd), [`66bd9a835`](https://github.com/medusajs/medusa/commit/66bd9a835c61b139af7051e5faf6c9de3c7134bb), [`4d16acf5f`](https://github.com/medusajs/medusa/commit/4d16acf5f096b5656b645f510f9c971e7c2dc9ef), [`4d16acf5f`](https://github.com/medusajs/medusa/commit/4d16acf5f096b5656b645f510f9c971e7c2dc9ef), [`87bade096`](https://github.com/medusajs/medusa/commit/87bade096e3d536f29ddc57dbc4c04e5d7a46e4b), [`4d16acf5f`](https://github.com/medusajs/medusa/commit/4d16acf5f096b5656b645f510f9c971e7c2dc9ef)]:
+  - @medusajs/types@1.11.0
+  - @medusajs/utils@1.10.0
+
+## 0.2.0
+
+### Minor Changes
+
+- [#4697](https://github.com/medusajs/medusa/pull/4697) [`c0ca00290`](https://github.com/medusajs/medusa/commit/c0ca00290106fbdc8e15077bc8d1c3eafbef59f2) Thanks [@carlos-r-l-rodrigues](https://github.com/carlos-r-l-rodrigues)! - Add pipe onComplete callback and preparation function to exportsWorkflow
+
+### Patch Changes
+
+- [#4716](https://github.com/medusajs/medusa/pull/4716) [`ac866ebb5`](https://github.com/medusajs/medusa/commit/ac866ebb5197ee694dda91824b501109012a3dd1) Thanks [@adrien2p](https://github.com/adrien2p)! - test(): Test the create product workflow compensation
+  fix(orchestration): Fix the transaction state after compensating with no compensation steps in the middle
+  chore(workflows): Export and naming
+  feat(types): Update product workflow input types
+  feat(medusa): Update product workflow usage and cleanup endpoint
+- Updated dependencies [[`3f3a84262`](https://github.com/medusajs/medusa/commit/3f3a84262ce9cbd911923278a54e301fbe9a4634), [`30ce35b16`](https://github.com/medusajs/medusa/commit/30ce35b163afa25f4e1d8d1bd392f401a3b413df)]:
+  - @medusajs/utils@1.9.6
+
+## 0.1.0
+
+### Minor Changes
+
+- [#4553](https://github.com/medusajs/medusa/pull/4553) [`f12299deb`](https://github.com/medusajs/medusa/commit/f12299deb10baadab1505cd4ac353dd5d1c8fa7c) Thanks [@carlos-r-l-rodrigues](https://github.com/carlos-r-l-rodrigues)! - Medusa workflows package
+
+### Patch Changes
+
+- Updated dependencies [[`131477faf`](https://github.com/medusajs/medusa/commit/131477faf0409c49d4aacf26ea591e33b2fa22fd)]:
+  - @medusajs/utils@1.9.3
+
+## 0.0.2
+
+### Patch Changes
+
+- [#4098](https://github.com/medusajs/medusa/pull/4098) [`499c3478c`](https://github.com/medusajs/medusa/commit/499c3478c910c8b922a15cc6f4d9fbad122a347f) Thanks [@carlos-r-l-rodrigues](https://github.com/carlos-r-l-rodrigues)! - feat: Remote Joiner
+
+- Updated dependencies [[`499c3478c`](https://github.com/medusajs/medusa/commit/499c3478c910c8b922a15cc6f4d9fbad122a347f), [`9dcdc0041`](https://github.com/medusajs/medusa/commit/9dcdc0041a2b08cc0723343dd8d9127d9977b086), [`9760d4a96`](https://github.com/medusajs/medusa/commit/9760d4a96c27f6f89a8c3f3b6e73b17547f97f2a)]:
+  - @medusajs/utils@1.9.2
