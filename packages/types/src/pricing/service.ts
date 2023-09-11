b72aba50e6e6a4c4b63ab20f88dacf8f72a3ef3a
Sammy filly <136061549+sammyfilly@users.noreply.github.com>
2023-09-11 22:54:12 +0100

    Initialize feat/add-price-set-rule-type

---
+++ packages/types/src/pricing/service.ts
@@ -0,0 +1,148 @@
+import { FindConfig } from "../common"
+import { ModuleJoinerConfig } from "../modules-sdk"
+import { Context } from "../shared-context"
+import {
+  CreateCurrencyDTO,
+  CreateMoneyAmountDTO,
+  CreatePriceSetDTO,
+  CurrencyDTO,
+  FilterableCurrencyProps,
+  FilterableMoneyAmountProps,
+  FilterablePriceSetProps,
+  MoneyAmountDTO,
+  PriceSetDTO,
+  UpdateCurrencyDTO,
+  UpdateMoneyAmountDTO,
+  UpdatePriceSetDTO,
+  CreateRuleTypeDTO,
+  FilterableRuleTypeProps,
+  RuleTypeDTO,
+  UpdateRuleTypeDTO,
+} from "./common"
+
+export interface IPricingModuleService {
+  __joinerConfig(): ModuleJoinerConfig
+
+  retrieve(
+    id: string,
+    config?: FindConfig<PriceSetDTO>,
+    sharedContext?: Context
+  ): Promise<PriceSetDTO>
+
+  list(
+    filters?: FilterablePriceSetProps,
+    config?: FindConfig<PriceSetDTO>,
+    sharedContext?: Context
+  ): Promise<PriceSetDTO[]>
+
+  listAndCount(
+    filters?: FilterablePriceSetProps,
+    config?: FindConfig<PriceSetDTO>,
+    sharedContext?: Context
+  ): Promise<[PriceSetDTO[], number]>
+
+  create(
+    data: CreatePriceSetDTO[],
+    sharedContext?: Context
+  ): Promise<PriceSetDTO[]>
+
+  update(
+    data: UpdatePriceSetDTO[],
+    sharedContext?: Context
+  ): Promise<PriceSetDTO[]>
+
+  delete(ids: string[], sharedContext?: Context): Promise<void>
+
+  retrieveMoneyAmount(
+    id: string,
+    config?: FindConfig<MoneyAmountDTO>,
+    sharedContext?: Context
+  ): Promise<MoneyAmountDTO>
+
+  listMoneyAmounts(
+    filters?: FilterableMoneyAmountProps,
+    config?: FindConfig<MoneyAmountDTO>,
+    sharedContext?: Context
+  ): Promise<MoneyAmountDTO[]>
+
+  listAndCountMoneyAmounts(
+    filters?: FilterableMoneyAmountProps,
+    config?: FindConfig<MoneyAmountDTO>,
+    sharedContext?: Context
+  ): Promise<[MoneyAmountDTO[], number]>
+
+  createMoneyAmounts(
+    data: CreateMoneyAmountDTO[],
+    sharedContext?: Context
+  ): Promise<MoneyAmountDTO[]>
+
+  updateMoneyAmounts(
+    data: UpdateMoneyAmountDTO[],
+    sharedContext?: Context
+  ): Promise<MoneyAmountDTO[]>
+
+  deleteMoneyAmounts(ids: string[], sharedContext?: Context): Promise<void>
+
+  retrieveCurrency(
+    code: string,
+    config?: FindConfig<CurrencyDTO>,
+    sharedContext?: Context
+  ): Promise<CurrencyDTO>
+
+  listCurrencies(
+    filters?: FilterableCurrencyProps,
+    config?: FindConfig<CurrencyDTO>,
+    sharedContext?: Context
+  ): Promise<CurrencyDTO[]>
+
+  listAndCountCurrencies(
+    filters?: FilterableCurrencyProps,
+    config?: FindConfig<CurrencyDTO>,
+    sharedContext?: Context
+  ): Promise<[CurrencyDTO[], number]>
+
+  createCurrencies(
+    data: CreateCurrencyDTO[],
+    sharedContext?: Context
+  ): Promise<CurrencyDTO[]>
+
+  updateCurrencies(
+    data: UpdateCurrencyDTO[],
+    sharedContext?: Context
+  ): Promise<CurrencyDTO[]>
+
+  deleteCurrencies(
+    currencyCodes: string[],
+    sharedContext?: Context
+  ): Promise<void>
+
+  retrieveRuleType(
+    code: string,
+    config?: FindConfig<RuleTypeDTO>,
+    sharedContext?: Context
+  ): Promise<RuleTypeDTO>
+
+  listRuleTypes(
+    filters?: FilterableRuleTypeProps,
+    config?: FindConfig<RuleTypeDTO>,
+    sharedContext?: Context
+  ): Promise<RuleTypeDTO[]>
+
+  listAndCountRuleTypes(
+    filters?: FilterableRuleTypeProps,
+    config?: FindConfig<RuleTypeDTO>,
+    sharedContext?: Context
+  ): Promise<[RuleTypeDTO[], number]>
+
+  createRuleTypes(
+    data: CreateRuleTypeDTO[],
+    sharedContext?: Context
+  ): Promise<RuleTypeDTO[]>
+
+  updateRuleTypes(
+    data: UpdateRuleTypeDTO[],
+    sharedContext?: Context
+  ): Promise<RuleTypeDTO[]>
+
+  deleteRuleTypes(ruleTypes: string[], sharedContext?: Context): Promise<void>
+}
