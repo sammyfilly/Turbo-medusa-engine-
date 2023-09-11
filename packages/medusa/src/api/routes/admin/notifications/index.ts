5b960e7e9fdae7717cba3f5350cad4f3efdffc4d
Sammy filly <136061549+sammyfilly@users.noreply.github.com>
2023-09-11 22:09:11 +0100

    Initialize chore/generate-api-ref-1676987570

---
+++ packages/medusa/src/api/routes/admin/notifications/index.ts
@@ -0,0 +1,64 @@
+import { Router } from "express"
+import { Notification } from "./../../../../"
+import middlewares from "../../../middlewares"
+
+const route = Router()
+
+export default (app) => {
+  app.use("/notifications", route)
+
+  /**
+   * List notifications
+   */
+  route.get("/", middlewares.wrap(require("./list-notifications").default))
+
+  /**
+   * Resend a notification
+   */
+  route.post(
+    "/:id/resend",
+    middlewares.wrap(require("./resend-notification").default)
+  )
+
+  return app
+}
+
+export const defaultAdminNotificationsRelations = ["resends"]
+
+export const defaultAdminNotificationsFields = [
+  "id",
+  "resource_type",
+  "resource_id",
+  "event_name",
+  "to",
+  "provider_id",
+  "created_at",
+  "updated_at",
+]
+
+/**
+ * @schema AdminNotificationsListRes
+ * type: object
+ * properties:
+ *   notifications:
+ *     type: array
+ *     items:
+ *       $ref: "#/components/schemas/Notification"
+ */
+export type AdminNotificationsListRes = {
+  notifications: Notification[]
+}
+
+/**
+ * @schema AdminNotificationsRes
+ * type: object
+ * properties:
+ *   notification:
+ *     $ref: "#/components/schemas/Notification"
+ */
+export type AdminNotificationsRes = {
+  notification: Notification
+}
+
+export * from "./list-notifications"
+export * from "./resend-notification"
