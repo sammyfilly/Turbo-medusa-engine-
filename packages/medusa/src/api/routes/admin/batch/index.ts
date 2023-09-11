864dc0bb47832a2df127ad79603ede4958cb6c37
Sammy filly <136061549+sammyfilly@users.noreply.github.com>
2023-09-11 21:31:09 +0100

    Initialize feat/batch-jobs-base-branch

---
+++ packages/medusa/src/api/routes/admin/batch/index.ts
@@ -0,0 +1,62 @@
+import { Router } from "express"
+import { BatchJob } from "../../../.."
+import { DeleteResponse, PaginatedResponse } from "../../../../types/common"
+import { AdminGetBatchParams } from "./list-batch-jobs"
+import middlewares, {
+  transformQuery,
+  getRequestedBatchJob,
+  canAccessBatchJob,
+} from "../../../middlewares"
+
+export default (app) => {
+  const route = Router()
+
+  app.use("/batch-jobs", route)
+
+  route.get(
+    "/",
+    transformQuery(AdminGetBatchParams, {
+      defaultFields: defaultAdminBatchFields,
+      isList: true,
+    }),
+    middlewares.wrap(require("./list-batch-jobs").default)
+  )
+  route.post("/", middlewares.wrap(require("./create-batch-job").default))
+
+  const routerOnBatch = Router()
+  route.use("/:id", getRequestedBatchJob, canAccessBatchJob, routerOnBatch)
+  routerOnBatch.get("/", middlewares.wrap(require("./get-batch-job").default))
+  routerOnBatch.post(
+    "/confirm",
+    middlewares.wrap(require("./confirm-batch-job").default)
+  )
+  routerOnBatch.post(
+    "/cancel",
+    middlewares.wrap(require("./cancel-batch-job").default)
+  )
+
+  return app
+}
+
+export type AdminBatchJobRes = {
+  batch_job: BatchJob
+}
+
+export type AdminBatchJobDeleteRes = DeleteResponse
+
+export type AdminBatchJobListRes = PaginatedResponse & {
+  batch_jobs: BatchJob[]
+}
+
+export const defaultAdminBatchFields = [
+  "id",
+  "type",
+  "context",
+  "result",
+  "created_by",
+  "created_at",
+  "updated_at",
+  "deleted_at",
+]
+
+export * from "./list-batch-jobs"
